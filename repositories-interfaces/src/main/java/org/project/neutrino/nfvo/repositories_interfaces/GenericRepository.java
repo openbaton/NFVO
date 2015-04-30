0c4ea291db4f40d10cb05917bcfa53110ca8902d
lorenzo.tomasini <lorenzo.tomasini@tu-berlin.de>
2015-04-30 18:47:46 +0200

    Adding module repositories and related interfaces, still working on it

---
+++ repositories-interfaces/src/main/java/org/project/neutrino/nfvo/repositories_interfaces/GenericRepository.java
@@ -0,0 +1,14 @@
+package org.project.neutrino.nfvo.repositories_interfaces;
+
+import java.util.List;
+
+/**
+ * Created by lto on 30/04/15.
+ */
+public interface GenericRepository<T>{
+    List<T> findAll();
+    T create(T entity);
+    T merge(T entity);
+    void remove(T entity);
+    T find(String id) throws ClassNotFoundException;
+}
