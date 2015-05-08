50e997c1c39fc31052b6b0707abbcb6ef8c42494
lorenzo.tomasini <lorenzo.tomasini@tu-berlin.de>
2015-05-08 11:26:30 +0200

    Added basic script for running and compiling neutrino

---
+++ neutrino.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+function start {
+
+    if [ ! -f build/libs/neutrino.jar  ]
+        then
+            ./gradlew build
+    fi
+
+    if [ 0 -eq $? ]
+        then
+            java -jar build/libs/neutrino.jar
+    fi
+}
+
+function compile {
+    ./gradlew build
+}
+
+function tests {
+    ./gradlew test
+}
+
+function end {
+    exit
+}
+function usage {
+    echo -e "neutrino Usage:\n\t ./neutrino.sh <option>\n\n\n\twhere option is\n\t\t * compile\n\t\t * start\n\t\t * test\n"
+}
+
+for var in "$@"
+do
+    case $var in
+        "sc" )
+            compile
+            start ;;
+        "start" )
+            start ;;
+        "compile" )
+            compile ;;
+        "test" )
+            tests ;;
+        * )
+            usage
+            end ;;
+    esac
+done
+
