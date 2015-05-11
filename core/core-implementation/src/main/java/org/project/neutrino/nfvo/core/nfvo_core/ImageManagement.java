9577f0ebacce828dd064588d96845bec9e80fdce
lorenzo.tomasini <lorenzo.tomasini@tu-berlin.de>
2015-05-11 16:28:06 +0200

    Implemented almost all ImageManagement

---
+++ core/core-implementation/src/main/java/org/project/neutrino/nfvo/core/nfvo_core/ImageManagement.java
@@ -0,0 +1,51 @@
+package org.project.neutrino.nfvo.core.nfvo_core;
+
+import org.project.neutrino.nfvo.catalogue.nfvo.Image;
+import org.project.neutrino.nfvo.repositories_interfaces.GenericRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+/**
+ * Created by lto on 11/05/15.
+ */
+public class ImageManagement implements org.project.neutrino.nfvo.core.interfaces.ImageManagement{
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    @Qualifier("imageRepository")
+    private GenericRepository<Image> imageRepository;
+
+    @Override
+    public void add(Image image) {
+        log.trace("Adding image " + image);
+        log.debug("Adding image with name " + image.getName());
+        imageRepository.create(image);
+    }
+
+    @Override
+    public void delete(String id) {
+        log.debug("Removing image with id " + id);
+        imageRepository.remove(imageRepository.find(id));
+    }
+
+    @Override
+    public Image update(Image new_image, String id) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public List<Image> query() {
+        return imageRepository.findAll();
+    }
+
+    @Override
+    public void copy() {
+        throw new NotImplementedException();
+    }
+}
