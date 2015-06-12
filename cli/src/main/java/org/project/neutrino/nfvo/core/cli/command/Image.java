aeb10832c2a1ecaef26714176e2c63c422e12737
Roman Busse <roman.busse@fokus.fraunhofer.de>
2015-06-13 01:52:00 +0200

    cli: fixed minor compilation problem due to missing import

--- cli/src/main/java/org/project/neutrino/nfvo/core/cli/command/Image.java
+++ cli/src/main/java/org/project/neutrino/nfvo/core/cli/command/Image.java
@@ -1,5 +1,6 @@
 package org.project.neutrino.nfvo.core.cli.command;
 
+import org.project.neutrino.nfvo.sdk.api.rest.ImageRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
