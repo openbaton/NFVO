cb80fc6048d991566a47f56d65c4472c2340e9b2
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-06-29 18:36:46 +0200

    removed security from nfvo since was causing problem with integration test

--- api/src/main/java/org/project/neutrino/nfvo/api/RestVimInstances.java
+++ api/src/main/java/org/project/neutrino/nfvo/api/RestVimInstances.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/v1/vim-instances")
+@RequestMapping("/api/v1/datacenters")
 public class RestVimInstances {
 
 //	TODO add log prints
