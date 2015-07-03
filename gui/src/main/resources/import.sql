98be9e59660da9cfbd86f553583293e66fcceb60
Giuseppe Carella <cargius@gmail.com>
2015-07-03 19:25:39 +0200

    removed gui --> moved on a new project dashboard

--- gui/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-insert into users (id, username, password, first_name, last_name, admin) values (1, 'mickey','$2a$10$kSqU.ek5pDRMMK21tHJlceS1xOc9Kna4F0DD2ZwQH/LAzH0ML0p6.','Mickey', 'Mouse',false);
-insert into users (id, username, password, first_name,last_name, admin) values (2, 'minnie','$2a$10$MnHcLn.XdLx.iMntXsmdgeO1B4wAW1E5GOy/VrLUmr4aAzabXnGFq','Minnie', 'Mouse', false);
-insert into users (id, username, password, first_name, last_name, admin) values (3, 'donald','$2a$10$0UCBI04PCXiK0pF/9kI7.uAXiHNQeeHdkv9NhA1/xgmRpfd4qxRMG','Donald', 'Duck', false);
-insert into users (id, username, password, first_name, last_name, admin) values (4, 'daisy','$2a$10$aNoR88g5b5TzSKb7mQ1nQOkyEwfHVQOxHY0HX7irI8qWINvLDWRyS', 'Daisy', 'Duck', false);
-insert into users (id, username, password, first_name, last_name, admin) values (5, 'clarabelle','$2a$10$cuTJd2ayEwXfsPdoF5/hde6gzsPx/gEiv8LZsjPN9VPoN5XVR8cKW','Clarabelle', 'Cow',false);
-insert into users (id, username, password, first_name, last_name, admin) values (6, 'admin', '$2a$10$JQOfG5Tqnf97SbGcKsalz.XpDQbXi1APOf2SHPVW27bWNioi9nI8y','Super','Admin',true);
