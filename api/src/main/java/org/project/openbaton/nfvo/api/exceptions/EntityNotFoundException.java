b6cd16d4d591ef41dd815051eb27229920d08051
Michael Pauls <michael.pauls@fokus.fraunhofer.de>
2015-07-10 14:57:51 +0200

    remove static messages + remove variable errors

--- api/src/main/java/org/project/openbaton/nfvo/api/exceptions/EntityNotFoundException.java
+++ api/src/main/java/org/project/openbaton/nfvo/api/exceptions/EntityNotFoundException.java
@@ -1,30 +1,14 @@
 package org.project.openbaton.nfvo.api.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @SuppressWarnings("serial")
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends RuntimeException {
 
-	private Errors errors;
-
-	public EntityNotFoundException(String message, Errors errors) {
-		super(message);
-		this.setErrors(errors);
-	}
-
 	public EntityNotFoundException(String message) {
 		super(message);
-	}
-
-	public Errors getErrors() {
-		return errors;
-	}
-
-	public void setErrors(Errors errors) {
-		this.errors = errors;
 	}
 
 }
