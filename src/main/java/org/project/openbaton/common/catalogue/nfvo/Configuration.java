1fefd93ee87c76a127168fd2a0a348a57aac0171
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-07-13 16:00:36 +0200

    moved catalogue

---
+++ src/main/java/org/project/openbaton/common/catalogue/nfvo/Configuration.java
@@ -0,0 +1,71 @@
+package org.project.openbaton.common.catalogue.nfvo;
+
+/**
+ * Created by lto on 18/05/15.
+ */
+
+import org.project.openbaton.common.catalogue.util.IdGenerator;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Configuration implements Serializable{
+    @Id
+    private String id = IdGenerator.createUUID();
+    @Version
+    private int version;
+
+    // TODO think at cascade type
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<ConfigurationParameter> parameters;
+
+    @Override
+    public String toString() {
+        return "Configuration{" +
+                "id='" + id + '\'' +
+                ", version=" + version +
+                ", parameters=" + parameters +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public List<ConfigurationParameter> getParameters() {
+    	if(parameters==null)
+			parameters = new ArrayList<ConfigurationParameter>();
+        return parameters;
+    }
+
+    public void setParameters(List<ConfigurationParameter> parameters) {
+        this.parameters = parameters;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
