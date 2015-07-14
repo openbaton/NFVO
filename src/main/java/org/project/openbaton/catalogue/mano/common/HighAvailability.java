a6726616b27c922cfb2e98e4babb4e95b1a52ae8
Domenico Bove <nicobb22@gmail.com>
2015-07-14 19:15:31 +0200

    renamed import

---
+++ src/main/java/org/project/openbaton/catalogue/mano/common/HighAvailability.java
@@ -0,0 +1,17 @@
+/*#############################################################################
+ # Copyright (c) 2015.                                                        #
+ #                                                                            #
+ # This file is part of the OpenSDNCore project.                              #
+ #############################################################################*/
+
+package org.project.openbaton.catalogue.mano.common;
+
+/**
+ * Created by lto on 06/02/15.
+ *
+ * Based on ETSI GS NFV-MAN 001 V1.1.1 (2014-12)
+ */
+public enum HighAvailability {
+    ACTIVE_ACTIVE,
+    ACTIVE_PASSIVE
+}
---
+++ src/main/java/org/project/openbaton/catalogue/nfvo/Action.java
@@ -0,0 +1,8 @@
+package org.project.openbaton.catalogue.nfvo;
+
+/**
+ * Created by lorenzo on 5/30/15.
+ */
+public enum Action {
+    INSTANTIATE_FINISH, ALLOCATE_RESOURCES, ERROR, RELEASE_RESOURCES, INSTANTIATE, MODIFY;
+}
