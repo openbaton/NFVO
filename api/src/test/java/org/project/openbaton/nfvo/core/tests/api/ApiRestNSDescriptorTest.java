76974620f96faefae274721c33138f4765a5c705
Giuseppe Carella <cargius@gmail.com>
2015-07-15 10:11:34 +0200

    Fixed wrong imports

--- api/src/test/java/org/project/openbaton/nfvo/core/tests/api/ApiRestNSDescriptorTest.java
+++ api/src/test/java/org/project/openbaton/nfvo/core/tests/api/ApiRestNSDescriptorTest.java
@@ -16,26 +16,21 @@ import org.junit.rules.ExpectedException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.project.openbaton.common.catalogue.mano.common.Security;
-import org.project.openbaton.common.catalogue.mano.common.VNFDependency;
-import org.project.openbaton.common.catalogue.mano.descriptor.NetworkServiceDescriptor;
-import org.project.openbaton.common.catalogue.mano.descriptor.PhysicalNetworkFunctionDescriptor;
-import org.project.openbaton.common.catalogue.mano.descriptor.VirtualNetworkFunctionDescriptor;
+import org.project.openbaton.catalogue.mano.common.Security;
+import org.project.openbaton.catalogue.mano.common.VNFDependency;
+
+
+import org.project.openbaton.catalogue.mano.descriptor.NetworkServiceDescriptor;
+import org.project.openbaton.catalogue.mano.descriptor.PhysicalNetworkFunctionDescriptor;
+import org.project.openbaton.catalogue.mano.descriptor.VirtualNetworkFunctionDescriptor;
 import org.project.openbaton.nfvo.api.RestNetworkServiceDescriptor;
 import org.project.openbaton.nfvo.api.exceptions.PNFDNotFoundException;
 import org.project.openbaton.nfvo.api.exceptions.VNFDNotFoundException;
 import org.project.openbaton.nfvo.api.exceptions.VNFDependencyNotFoundException;
-<<<<<<< HEAD
-import org.project.openbaton.catalogue.mano.common.Security;
-import org.project.openbaton.catalogue.mano.common.VNFDependency;
-import org.project.openbaton.catalogue.mano.descriptor.NetworkServiceDescriptor;
-import org.project.openbaton.catalogue.mano.descriptor.PhysicalNetworkFunctionDescriptor;
-import org.project.openbaton.catalogue.mano.descriptor.VirtualNetworkFunctionDescriptor;
-=======
->>>>>>> 1fe4f0d10746acdf01f0e14faa7760be701b1aa5
-import org.project.openbaton.nfvo.common.exceptions.BadFormatException;
-import org.project.openbaton.nfvo.exceptions.NotFoundException;
+
 import org.project.openbaton.nfvo.core.interfaces.NetworkServiceDescriptorManagement;
+import org.project.openbaton.nfvo.exceptions.BadFormatException;
+import org.project.openbaton.nfvo.exceptions.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -84,8 +79,7 @@ public class ApiRestNSDescriptorTest {
 	@Test
 	public void testNSDCreate() throws NotFoundException, BadFormatException {
 		when(nsdManagement.onboard(networkServiceDescriptor)).thenReturn(networkServiceDescriptor);
-		NetworkServiceDescriptor networkServiceDescriptor2 = null;
-		networkServiceDescriptor2 = restNetworkService.create(networkServiceDescriptor);
+		NetworkServiceDescriptor networkServiceDescriptor2  = restNetworkService.create(networkServiceDescriptor);
 		assertEquals(networkServiceDescriptor, networkServiceDescriptor2);
 	}
 
@@ -117,7 +111,7 @@ public class ApiRestNSDescriptorTest {
 	@Test
 	public void testpostVNFD() {
 
-		Set<VirtualNetworkFunctionDescriptor> list = new HashSet<VirtualNetworkFunctionDescriptor>();
+		Set<VirtualNetworkFunctionDescriptor> list = new HashSet<>();
 		networkServiceDescriptor.setVnfd(list);
 		VirtualNetworkFunctionDescriptor vnfd = new VirtualNetworkFunctionDescriptor();
 		vnfd.setName("test_VNFD");
@@ -215,7 +209,7 @@ public class ApiRestNSDescriptorTest {
 	@Test
 	public void testpostVNFDependency() {
 
-		Set<VNFDependency> list = new HashSet<VNFDependency>();
+		Set<VNFDependency> list = new HashSet<>();
 		networkServiceDescriptor.setVnf_dependency(list);
 		VNFDependency vnfd = new VNFDependency();
 
@@ -310,7 +304,7 @@ public class ApiRestNSDescriptorTest {
 	@Test
 	public void testpostPhysicalNetworkFunctionDescriptor() {
 
-		Set<PhysicalNetworkFunctionDescriptor> list = new HashSet<PhysicalNetworkFunctionDescriptor>();
+		Set<PhysicalNetworkFunctionDescriptor> list = new HashSet<>();
 		networkServiceDescriptor.setPnfd(list);
 		PhysicalNetworkFunctionDescriptor pnfd = new PhysicalNetworkFunctionDescriptor();
 
