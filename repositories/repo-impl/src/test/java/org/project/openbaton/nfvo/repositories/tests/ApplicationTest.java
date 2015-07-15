6be1bb0957d639eb72090f0c86022f142cd8d464
Giuseppe Carella <cargius@gmail.com>
2015-07-15 18:59:39 +0200

    added copyright header in all files

--- repositories/repo-impl/src/test/java/org/project/openbaton/nfvo/repositories/tests/ApplicationTest.java
+++ repositories/repo-impl/src/test/java/org/project/openbaton/nfvo/repositories/tests/ApplicationTest.java
@@ -1,10 +1,27 @@
+/*
+ * Copyright (c) 2015 Fraunhofer FOKUS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.project.openbaton.nfvo.repositories.tests;
 
+import org.project.openbaton.catalogue.mano.common.AbstractVirtualLink;
+import org.project.openbaton.catalogue.mano.descriptor.NetworkServiceDescriptor;
+import org.project.openbaton.catalogue.mano.record.NetworkServiceRecord;
+import org.project.openbaton.catalogue.nfvo.VimInstance;
 import org.project.openbaton.nfvo.abstract_repositories.DatabaseRepository;
-import org.project.openbaton.common.catalogue.nfvo.VimInstance;
-import org.project.openbaton.common.catalogue.mano.common.AbstractVirtualLink;
-import org.project.openbaton.common.catalogue.mano.descriptor.NetworkServiceDescriptor;
-import org.project.openbaton.common.catalogue.mano.record.NetworkServiceRecord;
+
 import org.project.openbaton.nfvo.repositories.NSDRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
