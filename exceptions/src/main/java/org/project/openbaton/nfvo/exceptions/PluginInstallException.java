67b8963a30ddb62af707748e9e780b4d57022e59
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-07-21 11:24:08 +0200

    implemented logic plugin install events

---
+++ exceptions/src/main/java/org/project/openbaton/nfvo/exceptions/PluginInstallException.java
@@ -0,0 +1,22 @@
+package org.project.openbaton.nfvo.exceptions;
+
+/**
+ * Created by lto on 21/07/15.
+ */
+public class PluginInstallException extends Exception{
+
+    public PluginInstallException() {
+    }
+
+    public PluginInstallException(String message) {
+        super(message);
+    }
+
+    public PluginInstallException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PluginInstallException(Throwable e) {
+        super(e);
+    }
+}
