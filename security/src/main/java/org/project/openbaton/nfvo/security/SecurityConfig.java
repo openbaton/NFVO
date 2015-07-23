812e0e7d819017008e9625a412c742628dbc9bd1
Domenico Bove <nicobb22@gmail.com>
2015-07-23 15:49:04 +0200

    Integrated the dashboard into nfvo project

--- security/src/main/java/org/project/openbaton/nfvo/security/SecurityConfig.java
+++ security/src/main/java/org/project/openbaton/nfvo/security/SecurityConfig.java
@@ -19,9 +19,11 @@ package org.project.openbaton.nfvo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -41,6 +43,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				new BCryptPasswordEncoder());
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").httpBasic()
+				.and()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll();
+	}
 	@Override
 	@Bean
 	protected AuthenticationManager
