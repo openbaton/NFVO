1f31963ea050c55ba4e98cd29dc20476c410fe36
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2015-07-24 19:09:33 +0200

    all test passed:)

--- repositories/repo-impl/src/test/java/org/project/openbaton/nfvo/repositories/tests/ApplicationTest.java
+++ repositories/repo-impl/src/test/java/org/project/openbaton/nfvo/repositories/tests/ApplicationTest.java
@@ -21,14 +21,14 @@ import org.project.openbaton.catalogue.mano.descriptor.NetworkServiceDescriptor;
 import org.project.openbaton.catalogue.mano.record.NetworkServiceRecord;
 import org.project.openbaton.catalogue.nfvo.VimInstance;
 import org.project.openbaton.nfvo.abstract_repositories.DatabaseRepository;
-
 import org.project.openbaton.nfvo.repositories.NSDRepository;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
@@ -37,7 +37,8 @@ import javax.sql.DataSource;
 /**
  * Created by lto on 30/04/15.
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EntityScan(basePackageClasses = {VimInstance.class, NetworkServiceDescriptor.class, AbstractVirtualLink.class, NetworkServiceRecord.class})
 @ComponentScan(basePackageClasses = {NSDRepository.class, DatabaseRepository.class})
 @EnableJpaRepositories(basePackageClasses = {NSDRepository.class, DatabaseRepository.class})
