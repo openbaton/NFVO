9d1deb311cc58cbff31794bc6eb3202fccfd4404
Tiziano Cecamore <cecamoretiziano25@gmail.com>
2015-08-06 16:54:18 +0200

    added version in interfaces

--- vim-drivers/src/main/java/org/project/openbaton/clients/interfaces/ClientInterfaces.java
+++ vim-drivers/src/main/java/org/project/openbaton/clients/interfaces/ClientInterfaces.java
@@ -14,6 +14,11 @@ import java.util.Set;
  */
 public interface ClientInterfaces {
 
+	/**
+	 * This version must match the version of the plugin...
+	 */
+	String interfaceVersion = "1.0";
+	
     public Server launchInstance(String name, String image, String flavor, String keypair, Set<String> network, Set<String> secGroup, String userData);
     public void init(VimInstance vimInstance);
 
