c474f63c26bc5aba2d2d0cc7884bda5b09279657
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-08-07 10:21:36 +0200

    first version of the TaskExecutor in the vnfmManager

--- vnfm/vnfm-impl/src/main/java/org/project/openbaton/nfvo/vnfm_reg/tasks/ScaleupfinishedTask.java
+++ vnfm/vnfm-impl/src/main/java/org/project/openbaton/nfvo/vnfm_reg/tasks/ScaleupfinishedTask.java
@@ -1,12 +1,15 @@
 package org.project.openbaton.nfvo.vnfm_reg.tasks;
 
 import org.project.openbaton.catalogue.mano.descriptor.VirtualDeploymentUnit;
+import org.project.openbaton.catalogue.mano.record.VirtualNetworkFunctionRecord;
 import org.project.openbaton.nfvo.vnfm_reg.tasks.abstracts.AbstractTask;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by lto on 06/08/15.
@@ -17,16 +20,25 @@ public class ScaleupfinishedTask extends AbstractTask {
     @Override
     protected void doWork() throws Exception {
         log.debug("NFVO: SCALE_UP_FINISHED");
-        log.debug("The VNFR: " + virtualNetworkFunctionRecord.getName() + " shoud be in status active --> " + virtualNetworkFunctionRecord.getStatus());
-//        virtualNetworkFunctionRecord.setStatus(Status.ACTIVE);
-        /**
-         * why?
-         */
+        VirtualNetworkFunctionRecord virtualNetworkFunctionRecord_nfvo = vnfrRepository.find(virtualNetworkFunctionRecord.getId());
+        virtualNetworkFunctionRecord_nfvo.setStatus(virtualNetworkFunctionRecord.getStatus());
         List<String> existingVDUs = new ArrayList<String>();
-        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord.getVdu()) {
+        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord_nfvo.getVdu()) {
             existingVDUs.add(vdu.getId());
         }
-        virtualNetworkFunctionRecord = vnfrRepository.merge(virtualNetworkFunctionRecord);
+        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord.getVdu()) {
+            if (!existingVDUs.contains(vdu.getId())) {
+                virtualNetworkFunctionRecord_nfvo.getVdu().add(vdu);
+            }
+        }
+        Set<String> new_addresses = new HashSet<>();
+        for (String ip : virtualNetworkFunctionRecord.getVnf_address()) {
+            if (!virtualNetworkFunctionRecord_nfvo.getVnf_address().contains(ip)) {
+                new_addresses.add(ip);
+            }
+        }
+        virtualNetworkFunctionRecord_nfvo.getVnf_address().addAll(new_addresses);
+        virtualNetworkFunctionRecord = vnfrRepository.merge(virtualNetworkFunctionRecord_nfvo);
     }
 
     @Override
