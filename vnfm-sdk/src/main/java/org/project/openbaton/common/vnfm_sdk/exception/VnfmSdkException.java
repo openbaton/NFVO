104419a22400ad3b6dec5b447082bb0943e1cc28
Marcello Monachesi <mob@buran.fokus.fraunhofer.de>
2015-08-11 16:25:13 +0200

    Added communication logic with EMS

---
+++ vnfm-sdk/src/main/java/org/project/openbaton/common/vnfm_sdk/exception/VnfmSdkException.java
@@ -0,0 +1,18 @@
+package org.project.openbaton.common.vnfm_sdk.exception;
+
+/**
+ * Created by mob on 11.08.15.
+ */
+public class VnfmSdkException extends Exception {
+    public VnfmSdkException() {
+        super();
+    }
+
+    public VnfmSdkException(String message) {
+        super(message);
+    }
+
+    public VnfmSdkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
