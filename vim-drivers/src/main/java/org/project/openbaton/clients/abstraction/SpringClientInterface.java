e6a76e6ccd4e74b4e6adcc21e454c88513ab8fdf
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2015-08-13 18:52:46 +0200

    first implementation of plugin abstract

---
+++ vim-drivers/src/main/java/org/project/openbaton/clients/abstraction/SpringClientInterface.java
@@ -0,0 +1,11 @@
+package org.project.openbaton.clients.abstraction;
+
+import org.project.openbaton.clients.interfaces.ClientInterfaces;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by lto on 13/08/15.
+ */
+@Service
+public abstract class SpringClientInterface implements ClientInterfaces{
+}
