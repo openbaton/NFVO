f3dd6018125bb7b285083bd03ebf6fdf122a2e07
Tiziano Cecamore <cecamoretiziano25@gmail.com>
2015-08-13 17:02:34 +0200

    invokeMethod through jms

--- common/src/main/java/org/project/openbaton/nfvo/common/interfaces/PluginAgent.java
+++ common/src/main/java/org/project/openbaton/nfvo/common/interfaces/PluginAgent.java
@@ -6,5 +6,5 @@ import java.lang.reflect.Method;
  * Created by tce on 13.08.15.
  */
 public interface PluginAgent {
-    void invokeMethod(Method method, Class inter, Object... parameters);
+    public <T> T invokeMethod(Method method, Class inter, Object... parameters);
 }
