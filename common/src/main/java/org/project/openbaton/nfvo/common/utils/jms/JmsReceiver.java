53a26050bdf17257bd2955bab067931aecc9d13f
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-08-14 16:28:37 +0200

    bugfixing

--- common/src/main/java/org/project/openbaton/nfvo/common/utils/jms/JmsReceiver.java
+++ common/src/main/java/org/project/openbaton/nfvo/common/utils/jms/JmsReceiver.java
@@ -4,7 +4,9 @@ import org.project.openbaton.nfvo.common.interfaces.Receiver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -15,6 +17,8 @@ import java.io.Serializable;
 /**
  * Created by tce on 14.08.15.
  */
+@Service
+@Scope
 public class JmsReceiver implements Receiver {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
