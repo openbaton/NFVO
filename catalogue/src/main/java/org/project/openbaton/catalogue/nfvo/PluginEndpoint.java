8140296a7ce04533b7e5e97e67d594b3b3337272
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-08-18 10:44:45 +0200

    added version to Plugin endpoint

--- catalogue/src/main/java/org/project/openbaton/catalogue/nfvo/PluginEndpoint.java
+++ catalogue/src/main/java/org/project/openbaton/catalogue/nfvo/PluginEndpoint.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 public class PluginEndpoint extends Endpoint{
 
     private String interfaceClass;
+    private String interfaceVersion;
 
     @Override
     public String toString() {
@@ -26,5 +27,13 @@ public class PluginEndpoint extends Endpoint{
 
     public void setInterfaceClass(String interfaceClass) {
         this.interfaceClass = interfaceClass;
+    }
+
+    public void setInterfaceVersion(String interfaceVersion) {
+        this.interfaceVersion = interfaceVersion;
+    }
+
+    public String getInterfaceVersion() {
+        return interfaceVersion;
     }
 }
