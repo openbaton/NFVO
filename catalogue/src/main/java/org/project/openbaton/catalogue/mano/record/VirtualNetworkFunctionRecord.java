f10f00c9514f3f35866590b0685e2a62b5cd76de
Marcello Monachesi <marco.mantis@gmail.com>
2015-08-20 18:16:14 +0200

    Added method to check cyclic dependency and field cyclicDependency in vnfd and vnfr

--- catalogue/src/main/java/org/project/openbaton/catalogue/mano/record/VirtualNetworkFunctionRecord.java
+++ catalogue/src/main/java/org/project/openbaton/catalogue/mano/record/VirtualNetworkFunctionRecord.java
@@ -168,6 +168,9 @@ public class VirtualNetworkFunctionRecord implements Serializable {
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Configuration provides;
 
+    @JsonIgnore
+    private boolean cyclicDependency;
+
     public VirtualNetworkFunctionRecord() {
         this.lifecycle_event = new HashSet<LifecycleEvent>();
     }
@@ -178,6 +181,14 @@ public class VirtualNetworkFunctionRecord implements Serializable {
 
     public void setEndpoint(String endpoint) {
         this.endpoint = endpoint;
+    }
+
+    public boolean hasCyclicDependency() {
+        return cyclicDependency;
+    }
+
+    public void setCyclicDependency(boolean cyclicDependency) {
+        this.cyclicDependency = cyclicDependency;
     }
 
     public String getParent_ns_id() {
