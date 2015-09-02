76f0c0e676a311eba2753e136513839d55e1732e
Michael Pauls <michael.pauls@fokus.fraunhofer.de>
2015-09-02 16:49:13 +0200

    updated tasks: ScaledownfinishedTask, ScaleupfinishedTask

--- vnfm/vnfm-impl/src/main/java/org/project/openbaton/nfvo/vnfm_reg/tasks/ScaleupfinishedTask.java
+++ vnfm/vnfm-impl/src/main/java/org/project/openbaton/nfvo/vnfm_reg/tasks/ScaleupfinishedTask.java
@@ -20,25 +20,7 @@ public class ScaleupfinishedTask extends AbstractTask {
     @Override
     protected void doWork() throws Exception {
         log.debug("NFVO: SCALE_UP_FINISHED");
-        VirtualNetworkFunctionRecord virtualNetworkFunctionRecord_nfvo = vnfrRepository.find(virtualNetworkFunctionRecord.getId());
-        virtualNetworkFunctionRecord_nfvo.setStatus(virtualNetworkFunctionRecord.getStatus());
-        List<String> existingVDUs = new ArrayList<String>();
-        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord_nfvo.getVdu()) {
-            existingVDUs.add(vdu.getId());
-        }
-        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord.getVdu()) {
-            if (!existingVDUs.contains(vdu.getId())) {
-                virtualNetworkFunctionRecord_nfvo.getVdu().add(vdu);
-            }
-        }
-        Set<String> new_addresses = new HashSet<>();
-        for (String ip : virtualNetworkFunctionRecord.getVnf_address()) {
-            if (!virtualNetworkFunctionRecord_nfvo.getVnf_address().contains(ip)) {
-                new_addresses.add(ip);
-            }
-        }
-        virtualNetworkFunctionRecord_nfvo.getVnf_address().addAll(new_addresses);
-        virtualNetworkFunctionRecord = vnfrRepository.merge(virtualNetworkFunctionRecord_nfvo);
+        virtualNetworkFunctionRecord = vnfrRepository.merge(virtualNetworkFunctionRecord);
     }
 
     @Override
