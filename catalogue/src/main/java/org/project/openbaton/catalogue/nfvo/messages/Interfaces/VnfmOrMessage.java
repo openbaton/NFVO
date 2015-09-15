58325bc4caafdda97adc52e50cf820e9104f5acf
Marcello Monachesi <mob@buran.fokus.fraunhofer.de>
2015-09-15 11:40:16 +0200

    Added NFVMessage structure

---
+++ catalogue/src/main/java/org/project/openbaton/catalogue/nfvo/messages/Interfaces/VnfmOrMessage.java
@@ -0,0 +1,7 @@
+package org.project.openbaton.catalogue.nfvo.messages.Interfaces;
+
+/**
+ * Created by mob on 14.09.15.
+ */
+public interface VnfmOrMessage extends NFVMessage{
+}
---
+++ catalogue/src/main/java/org/project/openbaton/catalogue/nfvo/messages/VnfmOrGenericMessage.java
@@ -0,0 +1,49 @@
+package org.project.openbaton.catalogue.nfvo.messages;
+
+import org.project.openbaton.catalogue.mano.record.VNFRecordDependency;
+import org.project.openbaton.catalogue.mano.record.VirtualNetworkFunctionRecord;
+import org.project.openbaton.catalogue.nfvo.Action;
+import org.project.openbaton.catalogue.nfvo.messages.Interfaces.VnfmOrMessage;
+
+/**
+ * Created by mob on 14.09.15.
+ */
+public class VnfmOrGenericMessage implements VnfmOrMessage {
+    private Action action;
+    private VirtualNetworkFunctionRecord virtualNetworkFunctionRecord;
+    private VNFRecordDependency vnfRecordDependency;
+
+
+
+    public VnfmOrGenericMessage(VirtualNetworkFunctionRecord vnfr, Action action) {
+        this.virtualNetworkFunctionRecord = vnfr;
+        this.action=action;
+
+    }
+
+    public VirtualNetworkFunctionRecord getVirtualNetworkFunctionRecord() {
+        return virtualNetworkFunctionRecord;
+    }
+
+    public void setVirtualNetworkFunctionRecord(VirtualNetworkFunctionRecord vnfr) {
+        this.virtualNetworkFunctionRecord = vnfr;
+    }
+    public VNFRecordDependency getVnfRecordDependency() {
+        return vnfRecordDependency;
+    }
+
+    public void setVnfRecordDependency(VNFRecordDependency vnfRecordDependency) {
+        this.vnfRecordDependency = vnfRecordDependency;
+    }
+    @Override
+    public String toString() {
+        return "VnfmOrGenericMessage{" +
+                "vnfr=" + virtualNetworkFunctionRecord +
+                '}';
+    }
+
+    @Override
+    public Action getAction() {
+        return action;
+    }
+}
