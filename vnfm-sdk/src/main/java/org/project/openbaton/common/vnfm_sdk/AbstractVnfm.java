e38ee22878057c6bd97f373f5261732297fc5ec5
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-09-26 16:47:27 +0200

    cleaned vnfm-sdk

--- vnfm-sdk/src/main/java/org/project/openbaton/common/vnfm_sdk/AbstractVnfm.java
+++ vnfm-sdk/src/main/java/org/project/openbaton/common/vnfm_sdk/AbstractVnfm.java
@@ -1,5 +1,6 @@
 package org.project.openbaton.common.vnfm_sdk;
 
+import org.project.openbaton.catalogue.mano.common.Ip;
 import org.project.openbaton.catalogue.mano.descriptor.InternalVirtualLink;
 import org.project.openbaton.catalogue.mano.descriptor.VirtualDeploymentUnit;
 import org.project.openbaton.catalogue.mano.descriptor.VirtualNetworkFunctionDescriptor;
@@ -220,49 +221,24 @@ public abstract class AbstractVnfm implements VNFLifecycleManagement, VNFLifecyc
 
     protected abstract void checkEmsStarted(String vduHostname);
 
-//    protected abstract Future<VirtualNetworkFunctionRecord> grantLifecycleOperation(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws VnfmSdkException;
-//
-//    protected abstract Future<VirtualNetworkFunctionRecord> allocateResources(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws VnfmSdkException;
-
     private void setupProvides(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) {
         fillSpecificProvides(virtualNetworkFunctionRecord);
 
         log.debug("Provides is: " + virtualNetworkFunctionRecord.getProvides());
         //TODO add common parameters, even not defined into the provides: i.e. ip (DONE ?)
 
-        List<String> hostnames = new ArrayList<>();
         for (VirtualDeploymentUnit virtualDeploymentUnit : virtualNetworkFunctionRecord.getVdu()) {
             for (VNFCInstance vnfcInstance : virtualDeploymentUnit.getVnfc_instance()) {
-                hostnames.add(vnfcInstance.getHostname());
+                int i = 1;
+                for (Ip ip : vnfcInstance.getIps()) {
+                    ConfigurationParameter cp = new ConfigurationParameter();
+                    cp.setConfKey(ip.getNetName() + i);
+                    cp.setValue(ip.getIp());
+                    virtualNetworkFunctionRecord.getProvides().getConfigurationParameters().add(cp);
+                    i++;
+                }
             }
         }
-        int i = 1;
-        for (String ip : virtualNetworkFunctionRecord.getVnf_address()) {
-            ConfigurationParameter cp = new ConfigurationParameter();
-            cp.setConfKey(/*virtualNetworkFunctionRecord.getType() + ".*/"ip" + i);
-            cp.setValue(ip);
-            i++;
-            virtualNetworkFunctionRecord.getProvides().getConfigurationParameters().add(cp);
-        }
-
-        ConfigurationParameter cp2 = new ConfigurationParameter();
-        cp2.setConfKey(virtualNetworkFunctionRecord.getType() + ".hostnames");
-        cp2.setValue(hostnames.toString());
-        virtualNetworkFunctionRecord.getProvides().getConfigurationParameters().add(cp2);
-        /**
-         * Before ending, need to get all the "provides" filled
-         *
-         * TODO ask EMS for specific parameters
-         *
-         */
-
-        log.debug("Provides is: " + virtualNetworkFunctionRecord.getProvides());
-        for (ConfigurationParameter configurationParameter : virtualNetworkFunctionRecord.getProvides().getConfigurationParameters()) {
-            if (!configurationParameter.getConfKey().startsWith("#nfvo:")) {
-                log.debug(configurationParameter.getConfKey() + ": " + configurationParameter.getValue());
-            }
-        }
-
     }
 
     /**
