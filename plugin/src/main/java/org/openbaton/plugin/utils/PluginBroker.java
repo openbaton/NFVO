70d66212d82caaedd4ce757037573e4567d1be40
Michael Pauls <michael.pauls@mailbox.tu-berlin.de>
2015-10-01 04:56:25 +0200

    updated methods

--- plugin/src/main/java/org/openbaton/plugin/utils/PluginBroker.java
+++ plugin/src/main/java/org/openbaton/plugin/utils/PluginBroker.java
@@ -26,8 +26,8 @@ import java.rmi.registry.Registry;
  */
 public class PluginBroker<T extends Remote> {
 
-    public T getPlugin(String inte, String type) throws RemoteException, NotBoundException {
-        Registry registry = getRegistry(1099);
+    public T getPlugin(String host, String inte, String type) throws RemoteException, NotBoundException {
+        Registry registry = getRegistry(host, 1099);
         return lookupPlugin(inte, type, registry);
     }
 
@@ -40,20 +40,20 @@ public class PluginBroker<T extends Remote> {
         throw new NotBoundException("plugin of type " + type + " not registered");
     }
 
-    private Registry getRegistry(int port) throws RemoteException {
-        return LocateRegistry.getRegistry(port);
+    private Registry getRegistry(String host,int port) throws RemoteException {
+        return LocateRegistry.getRegistry(host, port);
     }
 
-    public T getPlugin(String inte, String type, int port) throws RemoteException, NotBoundException {
-        return lookupPlugin(inte, type, getRegistry(port));
+    public T getPlugin(String host, String inte, String type, int port) throws RemoteException, NotBoundException {
+        return lookupPlugin(inte, type, getRegistry(host, port));
     }
 
-    public T getPlugin(String inte, String type, String name) throws RemoteException, NotBoundException {
-        return (T) getRegistry(1099).lookup(inte + "." + type + "." + name);
+    public T getPlugin(String host, String inte, String type, String name) throws RemoteException, NotBoundException {
+        return (T) getRegistry(host, 1099).lookup(inte + "." + type + "." + name);
     }
 
-    public T getPlugin(String inte, String type, String name, int port) throws RemoteException, NotBoundException {
+    public T getPlugin(String host, String inte, String type, String name, int port) throws RemoteException, NotBoundException {
 
-        return (T) getRegistry(port).lookup(inte + "." + type + "." + name);
+        return (T) getRegistry(host, port).lookup(inte + "." + type + "." + name);
     }
 }
