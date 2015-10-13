fb30400dc7dd1c7b1047d135d71d696e722d6739
Domenico Bove <nicobb22@gmail.com>
2015-10-13 11:11:51 +0200

    changed urls

--- src/main/resources/static/js/controllers/nsrController.js
+++ src/main/resources/static/js/controllers/nsrController.js
@@ -2,7 +2,7 @@ var app = angular.module('app').controller('NsrCtrl', function ($scope, $compile
 
     var url = '/api/v1/ns-records';
     //var url = 'http://localhost:8080/api/v1/ns-records';
-    //var url ='http://80.96.122.80:8080/api/v1/ns-records';
+
 
 
     loadTable();
