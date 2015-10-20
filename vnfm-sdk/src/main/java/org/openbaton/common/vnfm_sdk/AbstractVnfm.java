9d2b6b9da417ec931f695d411816bbf3f6f6ba2a
Michael Pauls <michael.pauls@mailbox.tu-berlin.de>
2015-10-20 17:42:00 +0200

    updated code

--- vnfm-sdk/src/main/java/org/openbaton/common/vnfm_sdk/AbstractVnfm.java
+++ vnfm-sdk/src/main/java/org/openbaton/common/vnfm_sdk/AbstractVnfm.java
@@ -172,7 +172,7 @@ public abstract class AbstractVnfm implements VNFLifecycleManagement, VNFLifecyc
                     VNFCInstance vnfcInstance_new = null;
                     for (VirtualDeploymentUnit virtualDeploymentUnit : virtualNetworkFunctionRecord.getVdu()) {
                         for (VNFCInstance vnfcInstance : virtualDeploymentUnit.getVnfc_instance())
-                                if (vnfcInstance.getVnfc_reference().equals(component.getId())) {
+                                if (vnfcInstance_new.getVnfComponent().getId().equals(component.getId())) {
                                     vnfcInstance_new = vnfcInstance;
                                     fillProvidesVNFC(virtualNetworkFunctionRecord, vnfcInstance);
                                     found = true;
