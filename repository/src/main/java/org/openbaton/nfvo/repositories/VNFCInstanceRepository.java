bb394739db09e522e36c8d688c2a68f6905761b8
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-10-22 14:53:02 +0200

    chaged Floatingips

---
+++ repository/src/main/java/org/openbaton/nfvo/repositories/VNFCInstanceRepository.java
@@ -0,0 +1,10 @@
+package org.openbaton.nfvo.repositories;
+
+import org.openbaton.catalogue.mano.record.VNFCInstance;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by lto on 22/10/15.
+ */
+public interface VNFCInstanceRepository extends CrudRepository<VNFCInstance,String> {
+}
