a8fc83910bdf0e6068c3b6415ed31bc9f7ddd96e
Marcello Monachesi <marco.mantis@gmail.com>
2015-10-26 18:51:57 +0100

    Added Alarm Entity and info related

---
+++ catalogue/src/main/java/org/openbaton/catalogue/mano/common/faultmanagement/PerceivedSeverity.java
@@ -0,0 +1,11 @@
+package org.openbaton.catalogue.mano.common.faultmanagement;
+
+/**
+ * Created by mob on 26.10.15.
+ */
+public enum PerceivedSeverity {
+    CRITICAL,
+    MAJOR,
+    MINOR,
+    WARNING
+}
