3db0e2133cd6ee2daf4c848fc2449451b1e50b49
Marcello Monachesi <marcello.monachesi@fokus.fraunhofer.de>
2015-11-02 11:52:03 +0100

    Added Fault Monitor test

--- catalogue/src/main/java/org/openbaton/catalogue/mano/common/faultmanagement/Criteria.java
+++ catalogue/src/main/java/org/openbaton/catalogue/mano/common/faultmanagement/Criteria.java
@@ -19,9 +19,7 @@ public class Criteria implements Serializable {
     private int version = 0;
 
     private String name;
-    private String vdu_selector;
     private Metric parameter_ref;
-    private String statistic;
     private String comparison_operator;
     private String threshold;
 
@@ -64,34 +62,15 @@ public class Criteria implements Serializable {
         this.threshold = threshold;
     }
 
-    public String getVdu_selector() {
-        return vdu_selector;
-    }
-
-    public void setVdu_selector(String vdu_selector) {
-        this.vdu_selector = vdu_selector;
-    }
-
-    public String getStatistic() {
-        return statistic;
-    }
-
-    public void setStatistic(String statistic) {
-        this.statistic = statistic;
-    }
-
     @Override
     public String toString() {
         return "Criteria{" +
                 "id='" + id + '\'' +
                 ", version=" + version +
                 ", name='" + name + '\'' +
-                ", vdu_selector='" + vdu_selector + '\'' +
                 ", parameter_ref=" + parameter_ref +
-                ", statistic='" + statistic + '\'' +
                 ", comparison_operator='" + comparison_operator + '\'' +
                 ", threshold='" + threshold + '\'' +
                 '}';
     }
-
 }
