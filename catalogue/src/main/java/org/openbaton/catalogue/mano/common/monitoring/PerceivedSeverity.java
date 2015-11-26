7f0fc390a9f42c3b7978d1d035678c40d175203b
Marcello Monachesi <marcello.monachesi@fokus.fraunhofer.de>
2015-11-26 17:07:24 +0100

    Added interfaces for monitoring and edited pluginCaller respectively

---
+++ catalogue/src/main/java/org/openbaton/catalogue/mano/common/monitoring/PerceivedSeverity.java
@@ -0,0 +1,12 @@
+package org.openbaton.catalogue.mano.common.monitoring;
+
+/**
+ * Created by mob on 26.10.15.
+ */
+public enum PerceivedSeverity {
+    CRITICAL,
+    MAJOR,
+    MINOR,
+    WARNING,
+    INDETERMINATE
+}
--- monitoring/src/main/java/org/openbaton/monitoring/interfaces/MonitoringPluginCaller.java
+++ monitoring/src/main/java/org/openbaton/monitoring/interfaces/MonitoringPluginCaller.java
@@ -1,5 +1,6 @@
 package org.openbaton.monitoring.interfaces;
 
+import org.openbaton.catalogue.mano.common.monitoring.*;
 import org.openbaton.catalogue.nfvo.Item;
 import org.openbaton.exceptions.MonitoringException;
 import org.openbaton.exceptions.NotFoundException;
@@ -40,7 +41,7 @@ public class MonitoringPluginCaller extends MonitoringPlugin {
 
     private PluginCaller pluginCaller;
 
-    @Override
+    /*@Override
     public List<Item> getMeasurementResults(List<String> hostnames, List<String> metrics, String period) throws RemoteException, MonitoringException {
 
         List<Serializable> params = new ArrayList<>();
@@ -75,7 +76,7 @@ public class MonitoringPluginCaller extends MonitoringPlugin {
             throw new MonitoringException(e.getMessage());
         }
 
-    }
+    }*/
 
     public static void main(String[] args) throws IOException, TimeoutException, NotFoundException, ExecutionException, InterruptedException {
 
@@ -88,7 +89,7 @@ public class MonitoringPluginCaller extends MonitoringPlugin {
                 List<String> hosts = new ArrayList<>();
                 hosts.add("hostname1");
                 hosts.add("hostname2");
-                return new MonitoringPluginCaller("zabbix").getMeasurementResults(hosts, new ArrayList<String>(), "");
+                return null/* new MonitoringPluginCaller("zabbix").getMeasurementResults(hosts, new ArrayList<String>(), "")*/;
             }
         }
 
@@ -104,5 +105,180 @@ public class MonitoringPluginCaller extends MonitoringPlugin {
         System.out.println(fut1.get());
         System.out.println("Time ---> " + (new Date().getTime() - time)/1000);
 
+    }
+
+    @Override
+    public String subscribeForFault(AlarmEndpoint filter) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add(filter);
+
+        Serializable res = null;
+        try {
+            res = pluginCaller.executeRPC("subscribeForFault", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (String) res;
+    }
+
+    @Override
+    public String unsubscribeForFault(String alarmEndpointId) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add(alarmEndpointId);
+
+        Serializable res = null;
+        try {
+            res = pluginCaller.executeRPC("unsubscribeForFault", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (String) res;
+    }
+
+    @Override
+    public void notifyFault(AlarmEndpoint endpoint, AbstractVirtualizedResourceAlarm event) throws MonitoringException {
+        //It must not be called from a consumer
+    }
+
+    @Override
+    public List<Alarm> getAlarmList(String vnfId, PerceivedSeverity perceivedSeverity) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add(vnfId);
+        params.add(perceivedSeverity);
+
+        Serializable res = null;
+        try {
+            res = pluginCaller.executeRPC("getAlarmList", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (List<Alarm>) res;
+    }
+
+    @Override
+    public String createPMJob(ObjectSelection resourceSelector, List<String> performanceMetric, List<String> performanceMetricGroup, Integer collectionPeriod, Integer reportingPeriod) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add(resourceSelector);
+        params.add((Serializable) performanceMetric);
+        params.add((Serializable) performanceMetricGroup);
+        params.add(collectionPeriod);
+        params.add(reportingPeriod);
+
+        Serializable res = null;
+        try {
+            res = pluginCaller.executeRPC("createPMJob", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (String) res;
+    }
+
+    @Override
+    public List<String> deletePMJob(List<String> itemIdsToDelete) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add((Serializable)itemIdsToDelete);
+
+        Serializable res = null;
+        try {
+            res = pluginCaller.executeRPC("deletePMJob", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (List<String>) res;
+    }
+
+    @Override
+    public List<Item> queryPMJob(List<String> hostnames, List<String> metrics, String period) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add((Serializable) hostnames);
+        params.add((Serializable) metrics);
+        params.add(period);
+        Serializable res = null;
+        try {
+
+
+            res = pluginCaller.executeRPC("queryPMJob", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (List<Item>) res;
+    }
+
+    @Override
+    public void subscribe() {
+        //not yet implemented
+    }
+
+    @Override
+    public void notifyInfo() {
+        //not yet implemented
+    }
+
+    @Override
+    public String createThreshold(List<ObjectSelection> objectSelectors, String performanceMetric, ThresholdType thresholdType, ThresholdDetails thresholdDetails) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add((Serializable) objectSelectors);
+        params.add(performanceMetric);
+        params.add(thresholdType);
+        params.add(thresholdDetails);
+
+        Serializable res = null;
+        try {
+            res = pluginCaller.executeRPC("createThreshold", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (String) res;
+    }
+
+    @Override
+    public List<String> deleteThreshold(List<String> thresholdIds) throws MonitoringException {
+        List<Serializable> params = new ArrayList<>();
+        params.add((Serializable)thresholdIds);
+
+        Serializable res = null;
+        try {
+            res = pluginCaller.executeRPC("deleteThreshold", params, List.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (PluginException e) {
+            throw new MonitoringException(e.getMessage());
+        }
+        return (List<String>) res;
+    }
+
+    @Override
+    public void queryThreshold(String queryFilter) {
+        //not yet implemented
     }
 }
