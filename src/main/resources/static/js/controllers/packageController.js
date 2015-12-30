3013705ebca5481ad4869f2eb15d0858b40e6fc9
Domenico Bove <domenico.bove@fokus.fraunhofer.de>
2015-12-30 15:14:47 +0100

    Removed buttons start and delete into package's page because in conflict with send all button

--- src/main/resources/static/js/controllers/packageController.js
+++ src/main/resources/static/js/controllers/packageController.js
@@ -91,7 +91,7 @@ app.controller('PackageCtrl', function ($scope, serviceAPI, $routeParams, http, 
                 previewsContainer: "#previews", // Define the container to display the previews
                 headers: header,
                 init: function () {
-                    var submitButton = document.querySelector("#submit-all")
+                    var submitButton = document.querySelector("#submit-all");
                     myDropzone = this; // closure
 
                     submitButton.addEventListener("click", function () {
@@ -117,12 +117,7 @@ app.controller('PackageCtrl', function ($scope, serviceAPI, $routeParams, http, 
             });
 
 
-            myDropzone.on("addedfile", function (file) {
-                // Hookup the start button
-                file.previewElement.querySelector(".start").onclick = function () {
-                    myDropzone.enqueueFile(file);
-                };
-            });
+
 
 // Update the total progress bar
             myDropzone.on("totaluploadprogress", function (progress) {
@@ -133,8 +128,7 @@ app.controller('PackageCtrl', function ($scope, serviceAPI, $routeParams, http, 
                 // Show the total progress bar when upload starts
                 $('.progress .bar:first').opacity = "1";
 
-                // And disable the start button
-                file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+
             });
 
 // Hide the total progress bar when nothing's uploading anymore
@@ -144,9 +138,7 @@ app.controller('PackageCtrl', function ($scope, serviceAPI, $routeParams, http, 
             });
 
 
-            $(".start").onclick = function () {
-                myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
-            };
+
             $(".cancel").onclick = function () {
                 myDropzone.removeAllFiles(true);
             };
