4888ec20b8055a46eca27e0c378d93fa23d1e595
Michael Pauls <michael.pauls@fokus.fraunhofer.de>
2016-01-22 17:09:00 +0100

    introduced extended AutoScalePolicy

---
+++ catalogue/src/main/java/org/openbaton/catalogue/mano/common/ScalingAlarm.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2015 Fraunhofer FOKUS
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openbaton.catalogue.mano.common;
+
+import org.openbaton.catalogue.util.IdGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Version;
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * Created by mpa on 15/12/15.
+ */
+@Entity
+public class ScalingAlarm implements Serializable{
+	@Id
+	private String id;
+	@Version
+	private int version = 0;
+
+	private String metric;
+	private String statistic;
+	private String comparisonOperator;
+	private double threshold;
+
+	private double weight;
+
+	@PrePersist
+	public void ensureId() {
+		id = IdGenerator.createUUID();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public String getMetric() {
+		return metric;
+	}
+
+	public void setMetric(String metric) {
+		this.metric = metric;
+	}
+
+	public String getStatistic() {
+		return statistic;
+	}
+
+	public void setStatistic(String statistic) {
+		this.statistic = statistic;
+	}
+
+	public String getComparisonOperator() {
+		return comparisonOperator;
+	}
+
+	public void setComparisonOperator(String comparisonOperator) {
+		this.comparisonOperator = comparisonOperator;
+	}
+
+	public double getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(double threshold) {
+		this.threshold = threshold;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "ScalingAlarm{" +
+				"id='" + id + '\'' +
+				", version=" + version +
+				", metric='" + metric + '\'' +
+				", statistic='" + statistic + '\'' +
+				", comparisonOperator='" + comparisonOperator + '\'' +
+				", weight='" + weight + '\'' +
+				'}';
+	}
+}
