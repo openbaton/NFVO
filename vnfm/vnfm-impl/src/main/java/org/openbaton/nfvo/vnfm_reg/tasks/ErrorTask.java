c0dfcd951a94ceadb97728ef0bcfc285c97591bd
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-01-26 16:16:56 +0100

    fixed flooding of error of Rabbit in case of unhandled exception

--- vnfm/vnfm-impl/src/main/java/org/openbaton/nfvo/vnfm_reg/tasks/ErrorTask.java
+++ vnfm/vnfm-impl/src/main/java/org/openbaton/nfvo/vnfm_reg/tasks/ErrorTask.java
@@ -16,6 +16,7 @@
 
 package org.openbaton.nfvo.vnfm_reg.tasks;
 
+import org.openbaton.catalogue.mano.record.NetworkServiceRecord;
 import org.openbaton.catalogue.nfvo.messages.Interfaces.NFVMessage;
 import org.openbaton.nfvo.vnfm_reg.tasks.abstracts.AbstractTask;
 import org.openbaton.catalogue.mano.record.Status;
@@ -29,6 +30,14 @@ import org.springframework.stereotype.Service;
 @Scope("prototype")
 public class ErrorTask extends AbstractTask {
 
+    private Exception exception;
+
+    public void setNsrId(String nsrId) {
+        this.nsrId = nsrId;
+    }
+
+    private String nsrId;
+
     @Override
     public boolean isAsync() {
         return true;
@@ -36,16 +45,36 @@ public class ErrorTask extends AbstractTask {
 
     @Override
     public NFVMessage doWork() throws Exception {
-        try {
-            log.debug("Existing HBVerison: " + vnfrRepository.findFirstById(virtualNetworkFunctionRecord.getId()).getHb_version());
-        }catch (Exception e) {
-            if (log.isDebugEnabled())
-                log.error(e.getMessage(),e);
+        if (log.isDebugEnabled()){
+            log.error("ERROR from VNFM: ", this.getException());
+        }else log.error("ERROR from VNFM: " + this.getException().getMessage());
+
+        if (virtualNetworkFunctionRecord != null) {
+            try {
+                log.debug("Existing HBVerison: " + vnfrRepository.findFirstById(virtualNetworkFunctionRecord.getId()).getHb_version());
+            } catch (Exception e) {
+                if (log.isDebugEnabled())
+                    log.error(e.getMessage(), e);
+            }
+            log.debug("Received version: " + virtualNetworkFunctionRecord.getHb_version());
+            log.error("----> ERROR for VNFR: " + virtualNetworkFunctionRecord.getName());
+            virtualNetworkFunctionRecord.setStatus(Status.ERROR);
+            saveVirtualNetworkFunctionRecord();
+        }else {
+            log.error("Received Error from some VNFM. No VNFR was received, maybe the error came before the createVNFR? Check the VNFM Logs");
+            NetworkServiceRecord networkServiceRecord = networkServiceRecordRepository.findFirstById(nsrId);
+            networkServiceRecord.setStatus(Status.ERROR);
+            log.debug("Setting the NSR " + networkServiceRecord.getName() + " in state ERROR");
+            networkServiceRecordRepository.save(networkServiceRecord);
         }
-        log.debug("Received version: " + virtualNetworkFunctionRecord.getHb_version());
-        log.error("----> ERROR for VNFR: " + virtualNetworkFunctionRecord.getName());
-        virtualNetworkFunctionRecord.setStatus(Status.ERROR);
-        saveVirtualNetworkFunctionRecord();
         return null;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+
+    public Exception getException() {
+        return exception;
     }
 }
