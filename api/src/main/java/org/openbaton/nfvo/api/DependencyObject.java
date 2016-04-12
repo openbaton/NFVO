8b47693835f66da64945357ca26f891689311faf
Marcello Monachesi <marco.mantis@gmail.com>
2016-04-12 12:52:35 +0200

    Dependency object for fm-project containing source and target

---
+++ api/src/main/java/org/openbaton/nfvo/api/DependencyObject.java
@@ -0,0 +1,37 @@
+package org.openbaton.nfvo.api;
+
+/**
+ * Created by mob on 12.04.16.
+ */
+public class DependencyObject {
+    private String source;
+    private String target;
+
+    public DependencyObject(){
+
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    @Override
+    public String toString() {
+        return "DependencyObject{" +
+                "source='" + source + '\'' +
+                ", target='" + target + '\'' +
+                '}';
+    }
+}
