0f1324a7ffe2de3d6b90fbee7fc89f91a3bd9328
Domenico Bove <nicobb22@gmail.com>
2016-04-21 19:06:24 +0200

    Added PUT method to the CORS filter

--- security/src/main/java/org/openbaton/nfvo/security/SimpleCorsFilter.java
+++ security/src/main/java/org/openbaton/nfvo/security/SimpleCorsFilter.java
@@ -36,7 +36,7 @@ public class SimpleCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type");
 
