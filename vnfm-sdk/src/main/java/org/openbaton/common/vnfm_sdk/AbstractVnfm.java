912dd95b3b544700b43e676dd578378b69589bbd
Marcello Monachesi <marco.mantis@gmail.com>
2016-07-26 17:49:47 +0200

    imp: reformat code

--- vnfm-sdk/src/main/java/org/openbaton/common/vnfm_sdk/AbstractVnfm.java
+++ vnfm-sdk/src/main/java/org/openbaton/common/vnfm_sdk/AbstractVnfm.java
@@ -439,9 +439,8 @@ public abstract class AbstractVnfm
       log.debug(
           "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       if (nfvMessage != null) {
-        log.debug(
-                "send " + nfvMessage.getClass().getSimpleName()+" to NFVO");
-                vnfmHelper.sendToNfvo(nfvMessage);
+        log.debug("send " + nfvMessage.getClass().getSimpleName() + " to NFVO");
+        vnfmHelper.sendToNfvo(nfvMessage);
       }
     } catch (Exception e) {
       log.error("ERROR: ", e);
