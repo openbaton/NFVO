9e8a8a0b855a9fd412825fead4cb4e75f3978bc8
flaviomu <flaviomurgiasrvcs@gmail.com>
2016-08-17 16:00:36 +0200

    fix: format correctly the code

--- catalogue/src/main/java/org/openbaton/catalogue/nfvo/messages/OrVnfmStartStopMessage.java
+++ catalogue/src/main/java/org/openbaton/catalogue/nfvo/messages/OrVnfmStartStopMessage.java
@@ -4,41 +4,39 @@ import org.openbaton.catalogue.mano.record.VNFCInstance;
 import org.openbaton.catalogue.mano.record.VirtualNetworkFunctionRecord;
 import org.openbaton.catalogue.nfvo.messages.Interfaces.OrVnfmMessage;
 
-
 /**
  * Created by fmu on 17/08/16.
  */
 public class OrVnfmStartStopMessage extends OrVnfmMessage {
 
-    private VNFCInstance vnfcInstance;
-    private VirtualNetworkFunctionRecord virtualNetworkFunctionRecord;
+  private VNFCInstance vnfcInstance;
+  private VirtualNetworkFunctionRecord virtualNetworkFunctionRecord;
 
-    @Override
-    public String toString() {
-        return "OrVnfmScalingMessage{"
-                + "virtualNetworkFunctionRecord="
-                + virtualNetworkFunctionRecord
-                + ", vnfcInstance="
-                + vnfcInstance
-                + "} "
-                + super.toString();
-    }
+  @Override
+  public String toString() {
+    return "OrVnfmScalingMessage{"
+        + "virtualNetworkFunctionRecord="
+        + virtualNetworkFunctionRecord
+        + ", vnfcInstance="
+        + vnfcInstance
+        + "} "
+        + super.toString();
+  }
 
-    public VirtualNetworkFunctionRecord getVirtualNetworkFunctionRecord() {
-        return virtualNetworkFunctionRecord;
-    }
+  public VirtualNetworkFunctionRecord getVirtualNetworkFunctionRecord() {
+    return virtualNetworkFunctionRecord;
+  }
 
-    public void setVirtualNetworkFunctionRecord(
-            VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) {
-        this.virtualNetworkFunctionRecord = virtualNetworkFunctionRecord;
-    }
+  public void setVirtualNetworkFunctionRecord(
+      VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) {
+    this.virtualNetworkFunctionRecord = virtualNetworkFunctionRecord;
+  }
 
-    public VNFCInstance getVnfcInstance() {
-        return vnfcInstance;
-    }
+  public VNFCInstance getVnfcInstance() {
+    return vnfcInstance;
+  }
 
-    public void setVnfcInstance(VNFCInstance vnfcInstance) {
-        this.vnfcInstance = vnfcInstance;
-    }
-
+  public void setVnfcInstance(VNFCInstance vnfcInstance) {
+    this.vnfcInstance = vnfcInstance;
+  }
 }
