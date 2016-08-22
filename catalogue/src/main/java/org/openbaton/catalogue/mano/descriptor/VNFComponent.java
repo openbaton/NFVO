a2d9c27de0bc2a31f5a325e47d197581e190d9d1
flaviomu <flaviomurgiasrvcs@gmail.com>
2016-08-22 10:43:31 +0200

    fix: remove equals and hashCode methods

--- catalogue/src/main/java/org/openbaton/catalogue/mano/descriptor/VNFComponent.java
+++ catalogue/src/main/java/org/openbaton/catalogue/mano/descriptor/VNFComponent.java
@@ -88,19 +88,4 @@ public class VNFComponent implements Serializable {
         + version
         + '}';
   }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    VNFComponent that = (VNFComponent) o;
-
-    return id.equals(that.id);
-  }
-
-  @Override
-  public int hashCode() {
-    return id.hashCode();
-  }
 }
