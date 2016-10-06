b7ff2cc25414beb2ca4f95f0af92e5e71e6d3b87
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2016-10-06 13:44:45 +0200

    imp: added version checking for vnfpackage

---
+++ exception/src/main/java/org/openbaton/exceptions/IncompatibleVNFPackage.java
@@ -0,0 +1,11 @@
+package org.openbaton.exceptions;
+
+/**
+ * Created by rvl on 05.10.16.
+ */
+public class IncompatibleVNFPackage extends Exception {
+
+  public IncompatibleVNFPackage(String message) {
+    super(message);
+  }
+}
