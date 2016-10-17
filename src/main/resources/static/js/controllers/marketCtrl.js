a06fb928afddd491269552630b69e1b7a0272eb9
Gozman Oleksandr <gozmanoleksandr@googlemail.com>
2016-10-17 12:37:43 +0200

    imp: updated dashboard for latest marketplace, trimmed vim tabsets

--- src/main/resources/static/js/controllers/marketCtrl.js
+++ src/main/resources/static/js/controllers/marketCtrl.js
@@ -74,7 +74,7 @@ $scope.closeAlert = function (index) {
 
 $scope.download = function(data) {
   $scope.requestlink = {};
-  $scope.requestlink['link'] = "http://" + $scope.marketUrl + "/api/v1/vnf-packages/" + data.id + "/download/";
+  $scope.requestlink['link'] = "http://" + $scope.marketUrl + "/api/v1/vnf-packages/" + data.id + "/tar/";
     console.log($scope.requestlink);
      http.post(url + "/api/v1/vnf-packages/marketdownload", JSON.stringify($scope.requestlink)).success(function (response) {
       showOk("The package is being downloaded");
