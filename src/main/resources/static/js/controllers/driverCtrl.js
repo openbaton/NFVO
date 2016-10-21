4d6f6812b6c7d8f36f8e5b7ae6763df058dd760a
Oleksandr Gozman <oleksandr.gozman@campus.tu-berlin.de>
2016-10-21 15:24:46 +0200

    fea: added NSDs from marketplace on GUI

--- src/main/resources/static/js/controllers/driverCtrl.js
+++ src/main/resources/static/js/controllers/driverCtrl.js
@@ -1,6 +1,7 @@
 var app = angular.module('app').controller('driverCtrl', function ($scope, serviceAPI, $routeParams, $http, $cookieStore, AuthService, $window, $interval, http) {
   var url =  $cookieStore.get('URL');
-  var defaultUrl = "marketplace.marketplace.org:8082";
+  //var defaultUrl = "lore:8082"
+  var defaultUrl = "marketplace.openbaton.org:8082";
   $scope.drivers = [];
   $scope.alerts = [];
   loadTable();
