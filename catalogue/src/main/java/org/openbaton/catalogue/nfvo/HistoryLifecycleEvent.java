36e23c9f068d7bdf802b68a752d4eaee3e7f3f9c
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-10-25 10:49:57 +0200

    Imp: added copyrights

--- catalogue/src/main/java/org/openbaton/catalogue/nfvo/HistoryLifecycleEvent.java
+++ catalogue/src/main/java/org/openbaton/catalogue/nfvo/HistoryLifecycleEvent.java
@@ -1,7 +1,25 @@
+/*
+ * Copyright (c) 2016 Open Baton (http://www.openbaton.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.openbaton.catalogue.nfvo;
 
 import org.openbaton.catalogue.util.IdGenerator;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
@@ -14,6 +32,7 @@ public class HistoryLifecycleEvent {
 
   @Id private String id;
   private String event;
+  @Column(length = 1024)
   private String description;
   private String executedAt;
 
