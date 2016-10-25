90f361fabc487cbfaaa033343826c419ea88bb84
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-10-25 12:39:47 +0200

    Fix: update migration version

--- main/src/main/java/org/openbaton/nfvo/system/FlywayConfig.java
+++ main/src/main/java/org/openbaton/nfvo/system/FlywayConfig.java
@@ -33,7 +33,7 @@ public class FlywayConfig {
     Flyway flyway = new Flyway();
     flyway.setDataSource(dataSource);
     flyway.setLocations("classpath:/flyway");
-    flyway.setBaselineVersion(MigrationVersion.fromVersion("2.2.0.2"));
+    flyway.setBaselineVersion(MigrationVersion.fromVersion("2.2.0.3"));
     try {
       flyway.baseline();
     } catch (FlywayException e) {
