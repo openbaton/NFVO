d859fe3073a07da6928dad910d192259b60d1c32
Gozman Oleksandr <gozmanoleksandr@googlemail.com>
2016-10-25 11:17:35 +0200

    fix: added license, minor fixes

--- src/main/resources/static/js/controllers/indexController.js
+++ src/main/resources/static/js/controllers/indexController.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2016 Open Baton (http://www.openbaton.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
 var app = angular.module('app');
 
 /**
@@ -41,12 +59,12 @@ app.controller('LoginController', function ($scope, AuthService, Session, $rootS
             .success(function (data) {
                 //console.log(data);
                 if (data === "false") {
-                    AuthService.loginGuest($scope.URL);
+                    return;
                 }
             })
             .error(function (data, status) {
                 if (status == 404) {
-                    AuthService.loginGuest($scope.URL);
+                    return;
                 }
                 console.info(('status != 404'));
                 console.error('Response error', status, data);
