5cb4b1a2a25f59abcdc3ff6300fbcb3d4665c113
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-10-27 17:28:08 +0200

    Update copyright

--- dashboard/src/main/resources/static/asset/js/plugins/bootstrap-material-datetimepicker.js
+++ dashboard/src/main/resources/static/asset/js/plugins/bootstrap-material-datetimepicker.js
@@ -1,10 +1,18 @@
-/*!
- * bootstrap-material-datetimepicker.js
+/*
+ * Copyright (c) 2016 Open Baton (http://www.openbaton.org)
  *
- * Copyright (c) 2015 Romain America
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Released under the MIT license
- * https://github.com/T00rk/bootstrap-material-datetimepicker/blob/gh-pages/LICENSE.md
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 (function ($, moment) {
