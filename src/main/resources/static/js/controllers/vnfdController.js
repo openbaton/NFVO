65a3b3a583f3bd62a1d785aaa836663f4915ac80
Oleksandr Gozman <oleksandr.gozman@campus.tu-berlin.de>
2016-11-18 22:45:15 +0100

    imp: added confkey added configurations key to creation of the vnfd

--- src/main/resources/static/js/controllers/vnfdController.js
+++ src/main/resources/static/js/controllers/vnfdController.js
@@ -39,7 +39,7 @@ var app = angular.module('app').controller('VnfdCtrl', function ($scope, $compil
       lifecycle_event:[],
       deployment_flavour:[{"flavour_key":"m1.small"}],
       auto_scale_policy:[],
-      configurations:{name:"conf_name", configurationParameters:[{"key":"value"}]} 
+      configurations:{name:"", configurationParameters:[]} 
    };
    $scope.custom_images = [];
    $scope.lifecycle_event_type = ["INSTANTIATE", "CONFIGURE","START", "TERMINATE", "SCALE_IN"];
@@ -235,7 +235,12 @@ var app = angular.module('app').controller('VnfdCtrl', function ($scope, $compil
         event.lifecycle_events.splice(index,1);
 
     };
-
+    $scope.addConfPar = function() {
+        $scope.vnfdCreate.configurations.configurationParameters.push({confKey:"",value:""});
+    };
+    $scope.removeConf = function(index) {
+        $scope.vnfdCreate.configurations.configurationParameters.splice(index, 1);
+    };
 
     $scope.addVNFD = function () {
         $http.get('descriptors/vnfd/vnfd.json')
