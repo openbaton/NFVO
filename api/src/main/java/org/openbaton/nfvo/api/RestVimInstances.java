978d8c65c3dab50a2142ec2b061cce742a675e55
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-12-12 10:37:03 +0100

    imp: Removed list import

--- api/src/main/java/org/openbaton/nfvo/api/RestVimInstances.java
+++ api/src/main/java/org/openbaton/nfvo/api/RestVimInstances.java
@@ -36,7 +36,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 import javax.validation.Valid;
