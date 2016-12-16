f0fa7304e536e4ff6442236451d856a27d16c22b
Giuseppe Carella <cargius@gmail.com>
2016-12-16 18:25:25 +0100

    fix: openbaton-libs formatting issues

--- vnfm-sdk-amqp/src/main/java/org/openbaton/common/vnfm_sdk/amqp/VnfmSpringHelperRabbit.java
+++ vnfm-sdk-amqp/src/main/java/org/openbaton/common/vnfm_sdk/amqp/VnfmSpringHelperRabbit.java
@@ -101,14 +101,14 @@ public class VnfmSpringHelperRabbit extends VnfmHelper {
     rabbitAdmin = new RabbitAdmin(connectionFactory);
     rabbitAdmin.declareExchange(new TopicExchange("openbaton-exchange"));
     rabbitAdmin.declareQueue(
-            new Queue(RabbitConfiguration.queueName_vnfmRegister, true, exclusive, autodelete));
+        new Queue(RabbitConfiguration.queueName_vnfmRegister, true, exclusive, autodelete));
     rabbitAdmin.declareBinding(
-            new Binding(
-                    RabbitConfiguration.queueName_vnfmRegister,
-                    Binding.DestinationType.QUEUE,
-                    "openbaton-exchange",
-                    RabbitConfiguration.queueName_vnfmRegister,
-                    null));
+        new Binding(
+            RabbitConfiguration.queueName_vnfmRegister,
+            Binding.DestinationType.QUEUE,
+            "openbaton-exchange",
+            RabbitConfiguration.queueName_vnfmRegister,
+            null));
   }
 
   public void sendMessageToQueue(String sendToQueueName, final Serializable message) {
@@ -127,9 +127,9 @@ public class VnfmSpringHelperRabbit extends VnfmHelper {
     rabbitTemplate.setReplyTimeout(timeout * 1000);
     rabbitTemplate.afterPropertiesSet();
     String response =
-            (String)
-                    this.rabbitTemplate.convertSendAndReceive(
-                            RabbitConfiguration.queueName_vnfmCoreActionsReply, gson.toJson(message));
+        (String)
+            this.rabbitTemplate.convertSendAndReceive(
+                RabbitConfiguration.queueName_vnfmCoreActionsReply, gson.toJson(message));
 
     return gson.fromJson(response, NFVMessage.class);
   }
@@ -142,16 +142,16 @@ public class VnfmSpringHelperRabbit extends VnfmHelper {
 
     log.debug("Sending to: " + queueName.toLowerCase().replace("_", "-"));
     String res =
-            (String)
-                    rabbitTemplate.convertSendAndReceive(
-                            "openbaton-exchange", queueName.toLowerCase().replace("_", "-"), message);
+        (String)
+            rabbitTemplate.convertSendAndReceive(
+                "openbaton-exchange", queueName.toLowerCase().replace("_", "-"), message);
     log.trace("Received from EMS: " + res);
     if (res == null) {
       log.error("After " + timeout + " seconds the ems did not answer.");
       throw new TimeoutException(
-              "After "
-                      + timeout
-                      + " seconds the ems did not answer. You can change this value by editing the application.properties propery \"vnfm.rabbitmq.sar.timeout\"");
+          "After "
+              + timeout
+              + " seconds the ems did not answer. You can change this value by editing the application.properties propery \"vnfm.rabbitmq.sar.timeout\"");
     }
     return res;
   }
