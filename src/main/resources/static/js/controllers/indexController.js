b544e72857ddec2ec9a0e28ec0e4e0464c6ec32a
Zohaib Hassan <zha@honshu.fokus.fraunhofer.de>
2017-04-06 10:47:03 +0200

    Fixed: Issue #71
    https://github.com/openbaton/dashboard/issues/71

--- src/main/resources/static/js/controllers/indexController.js
+++ src/main/resources/static/js/controllers/indexController.js
@@ -664,8 +664,60 @@ app.controller('IndexCtrl', function ($document, $scope, $compile, $routeParams,
 
     };
 
+        $("input[type=password]").keyup(function(){
+            var ucase = new RegExp("[A-Z]+");
+            var lcase = new RegExp("[a-z]+");
+            var num = new RegExp("[0-9]+");
 
+            if($("#newPassword").val().length >= 8){
+                $("#8char").removeClass("glyphicon-remove");
+                $("#8char").addClass("glyphicon-ok");
+                $("#8char").css("color","#00A41E");
+            }else{
+                $("#8char").removeClass("glyphicon-ok");
+                $("#8char").addClass("glyphicon-remove");
+                $("#8char").css("color","#FF0004");
+            }
 
+            if(ucase.test($("#newPassword").val())){
+                $("#ucase").removeClass("glyphicon-remove");
+                $("#ucase").addClass("glyphicon-ok");
+                $("#ucase").css("color","#00A41E");
+            }else{
+                $("#ucase").removeClass("glyphicon-ok");
+                $("#ucase").addClass("glyphicon-remove");
+                $("#ucase").css("color","#FF0004");
+            }
 
+            if(lcase.test($("#newPassword").val())){
+                $("#lcase").removeClass("glyphicon-remove");
+                $("#lcase").addClass("glyphicon-ok");
+                $("#lcase").css("color","#00A41E");
+            }else{
+                $("#lcase").removeClass("glyphicon-ok");
+                $("#lcase").addClass("glyphicon-remove");
+                $("#lcase").css("color","#FF0004");
+            }
+
+            if(num.test($("#newPassword").val())){
+                $("#num").removeClass("glyphicon-remove");
+                $("#num").addClass("glyphicon-ok");
+                $("#num").css("color","#00A41E");
+            }else{
+                $("#num").removeClass("glyphicon-ok");
+                $("#num").addClass("glyphicon-remove");
+                $("#num").css("color","#FF0004");
+            }
+
+            if(($("#newPassword").val() == $("#newPassword1").val() )&& $("#newPassword").val() !='' && $("#newPassword1").val()!='' ){
+                $("#pwmatch").removeClass("glyphicon-remove");
+                $("#pwmatch").addClass("glyphicon-ok");
+                $("#pwmatch").css("color","#00A41E");
+            }else{
+                $("#pwmatch").removeClass("glyphicon-ok");
+                $("#pwmatch").addClass("glyphicon-remove");
+                $("#pwmatch").css("color","#FF0004");
+            }
+        });
 
 });
