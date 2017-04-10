1c86f03f14389d33bdb051a86a8a7c9398a44a31
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-04-10 16:11:25 +0200

    fea: start implementation of service and manager registration

---
+++ api/src/main/java/org/openbaton/nfvo/api/admin/RestComponents.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016 Open Baton (http://openbaton.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openbaton.nfvo.api.admin;
+
+import com.google.gson.JsonObject;
+import io.swagger.annotations.ApiOperation;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import javax.validation.Valid;
+import org.openbaton.catalogue.nfvo.ManagerCredentials;
+import org.openbaton.nfvo.security.interfaces.ComponentManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/components")
+public class RestComponents {
+
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @Autowired private ComponentManager componentManager;
+
+  /** Enable a new Service. this generates a new AES Key that must be used in the Service SDK */
+  @ApiOperation(
+    value = "Enable Service",
+    notes =
+        "Enable a new Service. this generates a new AES Key that must be used in the Service SDK"
+  )
+  @RequestMapping(
+    value = "/services",
+    method = RequestMethod.POST,
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+  )
+  @ResponseStatus(HttpStatus.CREATED)
+  public byte[] enableService(
+      @RequestHeader(value = "project-id") String projectId,
+      @RequestBody @Valid JsonObject serviceRegisterBody)
+      throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+    return componentManager.enableService(serviceRegisterBody, projectId);
+  }
+
+  /** Enable a new Manager. this generates a new Rabbit User that must be used in the Manager SDK */
+  @ApiOperation(
+    value = "Enable Manager",
+    notes =
+        "Enable a new Manager. this generates a Rabbit user that must be used in the Manager SDK"
+  )
+  @RequestMapping(
+    value = "/managers",
+    method = RequestMethod.POST,
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  @ResponseStatus(HttpStatus.CREATED)
+  public ManagerCredentials enableManager(
+      @RequestHeader(value = "project-id") String projectId,
+      @RequestBody @Valid JsonObject serviceRegisterBody)
+      throws IOException {
+
+    return componentManager.enableManager(serviceRegisterBody);
+  }
+}
---
+++ security/src/main/java/org/openbaton/nfvo/security/components/ComponentManager.java
@@ -0,0 +1,317 @@
+package org.openbaton.nfvo.security.components;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.openbaton.catalogue.nfvo.ManagerCredentials;
+import org.openbaton.catalogue.nfvo.ServiceCredentials;
+import org.openbaton.catalogue.nfvo.ServiceMetadata;
+import org.openbaton.nfvo.common.utils.KeyHelper;
+import org.openbaton.nfvo.repositories.ServiceRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.stereotype.Service;
+
+//import java.util.Base64;
+
+/** Created by lto on 04/04/2017. */
+@Service
+@ConfigurationProperties
+public class ComponentManager implements org.openbaton.nfvo.security.interfaces.ComponentManager {
+
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @Autowired private TokenStore tokenStore;
+  @Autowired private Gson gson;
+
+  @Value("${nfvo.security.service.token.validity:31556952}")
+  private int serviceTokenValidityDuration;
+
+  @Autowired private ServiceRepository serviceRepository;
+
+  @Value("${nfvo.rabbit.brokerIp:localhost}")
+  private String brokerIp;
+
+  @Value("${nfvo.rabbit.managementPort:15672}")
+  private String managementPort;
+
+  @Value("${spring.rabbitmq.password:openbaton}")
+  private String rabbitPassword;
+
+  @Value("${spring.rabbitmq.username:admin}")
+  private String rabbitUsername;
+
+  @Value("${spring.rabbitmq.vhost:openbaton}")
+  private String vhost;
+
+  /*
+   * Service related operations
+   */
+
+  @Override
+  public ServiceCredentials registerService(String body) {
+
+    ServiceMetadata service = null;
+    String unencryptedBody = null;
+    for (ServiceMetadata serviceMetadata : serviceRepository.findAll()) {
+      try {
+        unencryptedBody =
+            KeyHelper.decrypt(body, KeyHelper.restoreKey(serviceMetadata.getKeyValue()));
+      } catch (NoSuchPaddingException e) {
+        e.printStackTrace();
+        return null;
+      } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+        return null;
+      } catch (InvalidKeyException e) {
+        e.printStackTrace();
+        return null;
+      } catch (BadPaddingException e) {
+        e.printStackTrace();
+        return null;
+      } catch (IllegalBlockSizeException e) {
+        e.printStackTrace();
+        return null;
+      }
+
+      service = serviceMetadata;
+      break;
+    }
+
+    if (service == null) {
+      log.error("Please register first your Service in order to get the super duper secret key");
+      return null;
+    }
+
+    JsonObject bodyJson = gson.fromJson(unencryptedBody, JsonObject.class);
+
+    String serviceName = bodyJson.getAsJsonPrimitive("name").getAsString();
+    String action = bodyJson.getAsJsonPrimitive("action").getAsString();
+
+    if (!service.getName().equals(serviceName)) {
+      log.error("The Name does not match!");
+      return null;
+    }
+
+    if (action.toLowerCase().equals("register")) {
+
+      if (service.getStatus().toLowerCase().equals("active")) {
+        log.error("Service is already ACTIVE!");
+        return null;
+      }
+
+      ServiceCredentials serviceCredentials = new ServiceCredentials();
+
+      String token = getNewToken(serviceName);
+
+      try {
+        serviceCredentials.setToken(
+            KeyHelper.encryptToString(token, KeyHelper.restoreKey(service.getKeyValue())));
+      } catch (NoSuchPaddingException e) {
+        e.printStackTrace();
+      } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+      } catch (InvalidKeyException e) {
+        e.printStackTrace();
+      } catch (BadPaddingException e) {
+        e.printStackTrace();
+      } catch (IllegalBlockSizeException e) {
+        e.printStackTrace();
+      }
+      service.setStatus("active");
+      serviceRepository.save(service);
+      return serviceCredentials;
+    } else if (action.toLowerCase().equals("unregister")
+        || action.toLowerCase().equals("deregister")) {
+      service.setStatus("down");
+      serviceRepository.save(service);
+      log.info("Set status of service " + serviceName + " to down");
+      return null;
+    } else if (action.toLowerCase().equals("remove") || action.toLowerCase().equals("delete")) {
+      serviceRepository.delete(service);
+      log.info("Removed service " + serviceName);
+      return null;
+    } else {
+      log.error("Action " + action + " unknown!");
+      return null;
+    }
+  }
+
+  @Override
+  public byte[] enableService(JsonObject jsonObject, String projectId)
+      throws NoSuchAlgorithmException, IOException {
+    ServiceMetadata serviceMetadata = new ServiceMetadata();
+    serviceMetadata.setName(jsonObject.get("name").getAsString());
+    serviceMetadata.setStatus("down");
+    serviceMetadata.setKeyValue(KeyHelper.genKey().getEncoded());
+    log.debug("Saving ServiceMetadata: " + serviceMetadata);
+    serviceRepository.save(serviceMetadata);
+    return serviceMetadata.getKeyValue();
+  }
+
+  private String getNewToken(String serviceName) {
+    Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+    authorities.add(new SimpleGrantedAuthority("ADMIN"));
+
+    Map<String, String> requestParameters = new HashMap<>();
+    boolean approved = true;
+    Set<String> scope = new HashSet<>();
+    scope.add("scope");
+    Set<String> resourceIds = new HashSet<>();
+    Set<String> responseTypes = new HashSet<>();
+    responseTypes.add("code");
+    Map<String, Serializable> extensionProperties = new HashMap<>();
+
+    OAuth2Request oAuth2Request =
+        new OAuth2Request(
+            requestParameters,
+            serviceName,
+            authorities,
+            true,
+            scope,
+            resourceIds,
+            null,
+            responseTypes,
+            extensionProperties);
+
+    User userPrincipal =
+        new User(serviceName, "" + Math.random() * 1000, true, true, true, true, authorities);
+
+    UsernamePasswordAuthenticationToken authenticationToken =
+        new UsernamePasswordAuthenticationToken(userPrincipal, null, authorities);
+    OAuth2Authentication auth = new OAuth2Authentication(oAuth2Request, authenticationToken);
+
+    DefaultTokenServices tokenServices = new DefaultTokenServices();
+    tokenServices.setSupportRefreshToken(true);
+    tokenServices.setTokenStore(this.tokenStore);
+    tokenServices.setAccessTokenValiditySeconds(serviceTokenValidityDuration);
+
+    OAuth2AccessToken token = tokenServices.createAccessToken(auth);
+    return token.getValue();
+  }
+
+  /*
+   * Manager related operations
+   */
+
+  @Override
+  public ManagerCredentials enableManager(JsonObject body) throws IOException {
+    ManagerCredentials managerCredentials = new ManagerCredentials();
+
+    String username = body.get("type").getAsString();
+    String password = org.apache.commons.lang.RandomStringUtils.randomAlphanumeric(16);
+    String uri = "http://" + brokerIp + ":" + managementPort + "/api/users/" + username;
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setAccept(
+        new ArrayList<MediaType>() {
+          {
+            add(MediaType.APPLICATION_JSON);
+          }
+        });
+
+    //    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+    //    AuthScope authScope =
+    //        new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM);
+    //    credsProvider.setCredentials(
+    //        authScope, new UsernamePasswordCredentials(rabbitUsername, rabbitPassword));
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    //curl -u admin:openbaton -X PUT http://10.147.66.131:15672/api/users/name -d '{"password":"password", "tags":"administrator", "vhost":"openbaton"}' -H "Content-Type: application/json" -H "Accept:application/json"
+    HashMap<String, String> map = new HashMap<>();
+
+    map.put("password", password);
+    map.put("tags", "administrator");
+    map.put("vhost", vhost);
+    String pass = gson.toJson(map);
+    log.debug("Body is: " + pass);
+    org.apache.http.HttpEntity requestEntity = new StringEntity(pass, ContentType.APPLICATION_JSON);
+
+    HttpPut put = new HttpPut(uri);
+    String authStr = rabbitUsername + ":" + rabbitPassword;
+    String encoding = Base64.encodeBase64String(authStr.getBytes());
+    put.setHeader("Authorization", "Basic " + encoding);
+    put.setHeader(new BasicHeader("Accept", MediaType.APPLICATION_JSON_VALUE));
+    put.setHeader(new BasicHeader("Content-type", MediaType.APPLICATION_JSON_VALUE));
+    put.setEntity(requestEntity);
+
+    log.debug("Executing request: " + put.getMethod() + " on " + uri);
+
+    CloseableHttpResponse response = httpclient.execute(put);
+    log.debug(String.valueOf("Status: " + response.getStatusLine().getStatusCode()));
+    if (response.getStatusLine().getStatusCode() != 204) {
+      log.error("Error creating user: " + response.getStatusLine());
+      return null;
+    }
+
+    //curl -u admin:openbaton -X PUT http://10.147.66.131:15672/api/permissions/openbaton/name -d '{"configure":"
+    // (^name)", "write":"(^openbaton)|(^name)", "read":"(^name)"}' -H "Content-Type: application/json" -H
+    // "Accept:application/json"
+
+    uri =
+        "http://" + brokerIp + ":" + managementPort + "/api/permissions/" + vhost + "/" + username;
+    put = new HttpPut(uri);
+
+    String regexOpenbaton = "(^openbaton)";
+    String regexManager = "(^" + username + ")";
+    String regexBoth = regexOpenbaton + "|" + regexManager;
+    map = new HashMap<>();
+    map.put("configure", regexManager);
+    map.put("write", regexBoth);
+    map.put("read", regexManager);
+    String stringEntity = gson.toJson(map);
+
+    log.debug("Body is: " + stringEntity);
+    put.setHeader("Authorization", "Basic " + encoding);
+    put.setHeader(new BasicHeader("Accept", MediaType.APPLICATION_JSON_VALUE));
+    put.setHeader(new BasicHeader("Content-type", MediaType.APPLICATION_JSON_VALUE));
+    put.setEntity(new StringEntity(stringEntity, ContentType.APPLICATION_JSON));
+
+    log.debug("Executing request: " + put.getMethod() + " on " + uri);
+    httpclient.execute(put);
+    response = httpclient.execute(put);
+    log.debug(String.valueOf("Status: " + response.getStatusLine().getStatusCode()));
+    if (response.getStatusLine().getStatusCode() != 204) {
+      log.error("Error creating user: " + response.getStatusLine());
+      return null;
+    }
+
+    managerCredentials.setRabbitUsername(username);
+    managerCredentials.setRabbitPassword(password);
+    return managerCredentials;
+  }
+}
