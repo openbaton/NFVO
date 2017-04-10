3c2d17106e6c8c650466b34614cb964e0573f98a
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-04-10 16:12:44 +0200

    fea: implemented model for NFVO's service agent branch

---
+++ catalogue/src/main/java/org/openbaton/catalogue/nfvo/ServiceCredentials.java
@@ -0,0 +1,14 @@
+package org.openbaton.catalogue.nfvo;
+
+/** Created by lto on 04/04/2017. */
+public class ServiceCredentials {
+  private String token;
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public String getToken() {
+    return token;
+  }
+}
