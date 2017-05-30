c448380665513c2fd07a6385042d979ee9794570
Thomas Briedigkeit <thomas.briedigkeit@fokus.fraunhofer.de>
2017-05-30 15:34:57 +0200

    Imp: Use the rabbitmq guest user for registration

--- openbaton-libs/utils/src/main/java/org/openbaton/utils/rabbit/RabbitManager.java
+++ openbaton-libs/utils/src/main/java/org/openbaton/utils/rabbit/RabbitManager.java
@@ -21,65 +21,248 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.openbaton.exceptions.WrongStatusException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 /** Created by lto on 25/11/15. */
 @Service
 @Scope
 public class RabbitManager {
 
-  private static Logger log = LoggerFactory.getLogger(RabbitManager.class);
+    private static Logger log = LoggerFactory.getLogger(RabbitManager.class);
 
-  private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-  public static List<String> getQueues(
-      String brokerIp, String username, String password, String virtualHost, int managementPort)
-      throws IOException {
-    List<String> result = new ArrayList<>();
-    String encoding = Base64.encodeBase64String((username + ":" + password).getBytes());
-    HttpGet httpGet =
-        new HttpGet(
-            "http://"
-                + brokerIp
-                + ":"
-                + managementPort
-                + "/api/queues/"
-                + virtualHost.replace("/", "%2f"));
-    httpGet.setHeader("Authorization", "Basic " + encoding);
+    public static List<String> getQueues(
+            String brokerIp, String username, String password, String virtualHost, int managementPort)
+            throws IOException {
+        List<String> result = new ArrayList<>();
+        String encoding = Base64.encodeBase64String((username + ":" + password).getBytes());
+        HttpGet httpGet =
+                new HttpGet(
+                        "http://"
+                                + brokerIp
+                                + ":"
+                                + managementPort
+                                + "/api/queues/"
+                                + virtualHost.replace("/", "%2f"));
+        httpGet.setHeader("Authorization", "Basic " + encoding);
 
-    log.trace("executing request " + httpGet.getRequestLine());
-    HttpClient httpclient = HttpClients.createDefault();
-    HttpResponse response = httpclient.execute(httpGet);
-    HttpEntity entity = response.getEntity();
+        log.trace("executing request " + httpGet.getRequestLine());
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpResponse response = httpclient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
 
-    InputStreamReader inputStreamReader = new InputStreamReader(entity.getContent());
+        InputStreamReader inputStreamReader = new InputStreamReader(entity.getContent());
 
-    JsonArray array = gson.fromJson(inputStreamReader, JsonArray.class);
-    if (array != null)
-      for (JsonElement queueJson : array) {
-        String name = queueJson.getAsJsonObject().get("name").getAsString();
-        result.add(name);
-        log.trace("found queue: " + name);
-      }
-    //TODO check for errors
-    log.trace("found queues: " + result.toString());
-    return result;
-  }
+        JsonArray array = gson.fromJson(inputStreamReader, JsonArray.class);
+        if (array != null)
+            for (JsonElement queueJson : array) {
+                String name = queueJson.getAsJsonObject().get("name").getAsString();
+                result.add(name);
+                log.trace("found queue: " + name);
+            }
+        //TODO check for errors
+        log.trace("found queues: " + result.toString());
+        return result;
+    }
 
-  public static void main(String[] args) throws IOException {
-    System.out.println(getQueues("localhost", "admin", "openbaton", "/", 5672));
-  }
+    public static void createRabbitMqUser(
+            String rabbitUsername,
+            String rabbitPassword,
+            String brokerIp,
+            String managementPort,
+            String newUserName,
+            String newUserPwd,
+            String vHost)
+            throws IOException, WrongStatusException {
+        String uri = "http://" + brokerIp + ":" + managementPort + "/api/users/" + newUserName;
+        Gson gson = new Gson();
+
+        // TODO switch to SSL if possible
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        //curl -u admin:openbaton -X PUT http://10.147.66.131:15672/api/users/name -d '{"password":"password", "tags":"administrator", "vhost":"openbaton"}' -H "Content-Type: application/json" -H "Accept:application/json"
+
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("password", newUserPwd);
+        map.put("tags", "administrator");
+        map.put("vhost", vHost);
+        String pass = gson.toJson(map);
+        log.trace("Body is: " + pass);
+        org.apache.http.HttpEntity requestEntity = new StringEntity(pass, ContentType.APPLICATION_JSON);
+        HttpPut put = new HttpPut(uri);
+        String authStr = rabbitUsername + ":" + rabbitPassword;
+        String encoding = Base64.encodeBase64String(authStr.getBytes());
+        put.setHeader("Authorization", "Basic " + encoding);
+        put.setHeader(new BasicHeader("Accept", "application/json"));
+        put.setHeader(new BasicHeader("Content-type", "application/json"));
+        put.setEntity(requestEntity);
+
+        log.trace("Executing request: " + put.getMethod() + " on " + uri);
+
+        CloseableHttpResponse response = httpclient.execute(put);
+        log.trace(String.valueOf("Status: " + response.getStatusLine().getStatusCode()));
+        if (!(response.getStatusLine().getStatusCode() == 204 || response.getStatusLine().getStatusCode() == 201)) {
+            httpclient.close();
+            throw new WrongStatusException(
+                    "Error creating RabbitMQ user " + newUserName + ": " + response.getStatusLine());
+        }
+        httpclient.close();
+        //curl -u admin:openbaton -X PUT http://10.147.66.131:15672/api/permissions/openbaton/name -d '{"configure":"
+        // (^name)", "write":"(^openbaton)|(^name)", "read":"(^name)"}' -H "Content-Type: application/json" -H
+        // "Accept:application/json"
+
+    }
+
+    public static void setRabbitMqUserPermissions(
+            String rabbitUsername,
+            String rabbitPassword,
+            String brokerIp,
+            String managementPort,
+            String username,
+            String vHost,
+            String configurePermission,
+            String writePermission,
+            String readPermission)
+            throws IOException, WrongStatusException {
+        Gson gson = new Gson();
+        if (configurePermission == null) configurePermission = "";
+        if (writePermission == null) writePermission = "";
+        if (readPermission == null) readPermission = "";
+        String uri =
+                "http://"
+                        + brokerIp
+                        + ":"
+                        + managementPort
+                        + "/api/permissions/"
+                        + vHost.replace("/", "%2f")
+                        + "/"
+                        + username;
+        HttpPut put = new HttpPut(uri);
+
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("configure", configurePermission);
+        map.put("write", writePermission);
+        map.put("read", readPermission);
+        String stringEntity = gson.toJson(map);
+
+        log.trace("Body is: " + stringEntity);
+        String authStr = rabbitUsername + ":" + rabbitPassword;
+        String encoding = Base64.encodeBase64String(authStr.getBytes());
+        put.setHeader("Authorization", "Basic " + encoding);
+        put.setHeader(new BasicHeader("Accept", "application/json"));
+        put.setHeader(new BasicHeader("Content-type", "application/json"));
+        put.setEntity(new StringEntity(stringEntity, ContentType.APPLICATION_JSON));
+
+        // TODO switch to SSL if possible
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        log.trace("Executing request: " + put.getMethod() + " on " + uri);
+        httpclient.execute(put);
+        CloseableHttpResponse response = httpclient.execute(put);
+        log.trace(String.valueOf("Status: " + response.getStatusLine().getStatusCode()));
+        if (!(response.getStatusLine().getStatusCode() == 204 || response.getStatusLine().getStatusCode() == 201)) {
+            httpclient.close();
+            throw new WrongStatusException(
+                    "Error setting permissions of RabbitMQ user "
+                            + username
+                            + ": "
+                            + response.getStatusLine());
+        }
+        httpclient.close();
+    }
+
+    public static void removeRabbitMqUser(
+            String rabbitUsername,
+            String rabbitPassword,
+            String brokerIp,
+            String managementPort,
+            String userToRemove)
+            throws IOException, WrongStatusException {
+        String uri = "http://" + brokerIp + ":" + managementPort + "/api/users/" + userToRemove;
+
+        // TODO switch to SSL if possible
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        HttpDelete delete = new HttpDelete(uri);
+        String authStr = rabbitUsername + ":" + rabbitPassword;
+        String encoding = Base64.encodeBase64String(authStr.getBytes());
+        delete.setHeader("Authorization", "Basic " + encoding);
+        delete.setHeader(new BasicHeader("Accept", "application/json"));
+        //        delete.setHeader(new BasicHeader("Content-type", MediaType.APPLICATION_JSON_VALUE));
+
+        log.trace("Executing request: " + delete.getMethod() + " on " + uri);
+
+        CloseableHttpResponse response = httpclient.execute(delete);
+        log.trace(String.valueOf("Status: " + response.getStatusLine().getStatusCode()));
+        if (response.getStatusLine().getStatusCode() != 204) {
+            throw new WrongStatusException(
+                    "Error removing RabbitMQ user " + userToRemove + ": " + response.getStatusLine());
+        }
+    }
+
+    public static void createQueue(
+            String brokerIp,
+            int port,
+            String rabbitUsername,
+            String rabbitPassword,
+            String virtualHost,
+            String queue,
+            String exchange)
+            throws IOException, TimeoutException {
+        ConnectionFactory factory =
+                getConnectionFactory(brokerIp, port, rabbitUsername, rabbitPassword, virtualHost);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(exchange, "topic", true);
+        channel.queueDeclare(queue, false, false, true, null);
+        channel.queueBind(queue, exchange, queue);
+        channel.basicQos(1);
+        channel.close();
+        connection.close();
+    }
+
+    private static ConnectionFactory getConnectionFactory(
+            String brokerIp, int port, String rabbitUsername, String rabbitPassword, String virtualHost) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(brokerIp);
+        factory.setPort(port);
+        factory.setUsername(rabbitUsername);
+        factory.setPassword(rabbitPassword);
+        factory.setVirtualHost(virtualHost);
+        return factory;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getQueues("localhost", "admin", "openbaton", "/", 5672));
+    }
 }
--- openbaton-libs/vnfm-sdk-amqp/src/main/java/org/openbaton/common/vnfm_sdk/amqp/configuration/RabbitConfiguration.java
+++ openbaton-libs/vnfm-sdk-amqp/src/main/java/org/openbaton/common/vnfm_sdk/amqp/configuration/RabbitConfiguration.java
@@ -17,189 +17,19 @@
 
 package org.openbaton.common.vnfm_sdk.amqp.configuration;
 
-import java.io.IOException;
-import java.util.Properties;
-import javax.annotation.PostConstruct;
-import org.openbaton.common.vnfm_sdk.amqp.AbstractVnfmSpringAmqp;
 import org.openbaton.common.vnfm_sdk.interfaces.LogDispatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 
 /** Created by lto on 09/11/15. */
-@Configuration
-@EnableRabbit
-@ConfigurationProperties(prefix = "vnfm.rabbitmq")
-@Order(value = Ordered.HIGHEST_PRECEDENCE)
+// TODO consider removing this class once the new vnfm registration and communication stuff is stable
 public class RabbitConfiguration {
-  public static final String queueName_vnfmRegister = "nfvo.vnfm.register";
-  public static final String queueName_vnfmUnregister = "nfvo.vnfm.unregister";
+
   public static final String queueName_vnfmCoreActions = "vnfm.nfvo.actions";
   public static final String queueName_vnfmCoreActionsReply = "vnfm.nfvo.actions.reply";
-  public static String queueName_nfvoGenericActions = "nfvo.type.actions";
   private static final String queueName_logDispatch = "nfvo.vnfm.logs";
 
-  private RabbitAdmin rabbitAdmin;
 
-  private boolean autodelete = true;
-  private boolean durable;
-  private boolean exclusive;
-  private int minConcurrency;
-  private int maxConcurrency;
-
-  @Autowired private ConnectionFactory connectionFactory;
-
-  @Autowired(required = false)
-  private LogDispatcher logDispatcher;
-
-  private Logger log = LoggerFactory.getLogger(this.getClass());
-
-  public int getMaxConcurrency() {
-    return maxConcurrency;
-  }
-
-  public void setMaxConcurrency(int maxConcurrency) {
-    this.maxConcurrency = maxConcurrency;
-  }
-
-  public int getMinConcurrency() {
-    return minConcurrency;
-  }
-
-  public void setMinConcurrency(int minConcurrency) {
-    this.minConcurrency = minConcurrency;
-  }
-
-  public boolean isAutodelete() {
-    return autodelete;
-  }
-
-  public void setAutodelete(boolean autodelete) {
-    this.autodelete = autodelete;
-  }
-
-  public boolean isDurable() {
-    return durable;
-  }
-
-  public void setDurable(boolean durable) {
-    this.durable = durable;
-  }
-
-  public boolean isExclusive() {
-    return exclusive;
-  }
-
-  public void setExclusive(boolean exclusive) {
-    this.exclusive = exclusive;
-  }
-
-  @PostConstruct
-  private void init() {
-    log.info("Initialization of RabbitConfiguration");
-    rabbitAdmin = new RabbitAdmin(connectionFactory);
-    TopicExchange topicExchange = new TopicExchange("openbaton-exchange");
-    rabbitAdmin.declareExchange(topicExchange);
-    log.info("exchange declared");
-  }
-
-  @Bean
-  Queue queue_logDispatch() {
-    return new Queue(queueName_logDispatch, durable, exclusive, autodelete);
-  }
-
-  @Bean
-  Binding binding_logDispatch(TopicExchange exchange) {
-    return BindingBuilder.bind(queue_logDispatch()).to(exchange).with(queueName_logDispatch);
-  }
-
-  @Bean
-  MessageListenerAdapter listenerAdapter_logDispatch() {
-    if (logDispatcher != null) return new MessageListenerAdapter(logDispatcher, "sendLogs");
-    else return null;
-  }
-
-  @Bean
-  SimpleMessageListenerContainer container_logDispatcher(
-      ConnectionFactory connectionFactory,
-      @Qualifier("listenerAdapter_logDispatch") MessageListenerAdapter listenerAdapter) {
-    if (listenerAdapter != null) {
-      SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-      container.setConnectionFactory(connectionFactory);
-      container.setQueueNames(queueName_logDispatch);
-      if (minConcurrency <= 0 || maxConcurrency <= 0 || minConcurrency > maxConcurrency) {
-        container.setConcurrentConsumers(5);
-        container.setMaxConcurrentConsumers(15);
-      } else {
-        container.setConcurrentConsumers(minConcurrency);
-        container.setMaxConcurrentConsumers(maxConcurrency);
-      }
-      container.setMessageListener(listenerAdapter);
-      return container;
-    } else return null;
-  }
-
-  @Bean
-  Queue queue_genericVnfmActions() {
-    Properties properties = new Properties();
-
-    try {
-      properties.load(this.getClass().getClassLoader().getResourceAsStream("conf.properties"));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    queueName_nfvoGenericActions = "nfvo." + properties.getProperty("type") + ".actions";
-    return new Queue(queueName_nfvoGenericActions, durable, exclusive, autodelete);
-  }
-
-  @Bean
-  TopicExchange exchange() {
-    TopicExchange topicExchange = new TopicExchange("openbaton-exchange");
-    return topicExchange;
-  }
-
-  @Bean
-  Binding binding_nfvoGenericAction(TopicExchange exchange) {
-    return BindingBuilder.bind(queue_genericVnfmActions())
-        .to(exchange)
-        .with(queueName_nfvoGenericActions);
-  }
-
-  @Bean
-  MessageListenerAdapter listenerAdapter_nfvoGenericActions(AbstractVnfmSpringAmqp receiver) {
-    return new MessageListenerAdapter(receiver, "onAction");
-  }
-
-  @Bean
-  SimpleMessageListenerContainer container_eventRegister(
-      ConnectionFactory connectionFactory,
-      @Qualifier("listenerAdapter_nfvoGenericActions") MessageListenerAdapter listenerAdapter) {
-    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-    container.setConnectionFactory(connectionFactory);
-    container.setQueueNames(queueName_nfvoGenericActions);
-    if (minConcurrency <= 0 || maxConcurrency <= 0 || minConcurrency > maxConcurrency) {
-      container.setConcurrentConsumers(5);
-      container.setMaxConcurrentConsumers(15);
-    } else {
-      container.setConcurrentConsumers(minConcurrency);
-      container.setMaxConcurrentConsumers(maxConcurrency);
-    }
-    container.setMessageListener(listenerAdapter);
-    return container;
-  }
 }
