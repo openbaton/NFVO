b4e0d827f273ca363878d6238fd1a30ff2091757
Marcello Monachesi <marco.mantis@gmail.com>
2017-08-21 12:28:56 +0200

    fix: fix test with correct dependencies

--- main/src/test/java/org/openbaton/nfvo/core/test/IntegrationClassSuiteTest.java
+++ main/src/test/java/org/openbaton/nfvo/core/test/IntegrationClassSuiteTest.java
@@ -24,17 +24,17 @@ import org.openbaton.nfvo.main.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 /** Created by lto on 20/04/15. */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class})
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
-@ContextConfiguration(classes = {Application.class})
 @TestPropertySource(properties = {"timezone = GMT", "port: 4242"})
 public class IntegrationClassSuiteTest {
 
