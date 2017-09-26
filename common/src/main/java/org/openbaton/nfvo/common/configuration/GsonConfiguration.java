1a94aeea29058221cd6171a9a1c074bbd937658a
Marcello Monachesi <marco.mantis@gmail.com>
2017-09-26 12:15:01 +0200

    imp: remove unused annotation

--- common/src/main/java/org/openbaton/nfvo/common/configuration/GsonConfiguration.java
+++ common/src/main/java/org/openbaton/nfvo/common/configuration/GsonConfiguration.java
@@ -20,14 +20,12 @@ package org.openbaton.nfvo.common.configuration;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.openbaton.catalogue.nfvo.messages.Interfaces.NFVMessage;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 /** Created by lto on 10/11/15. */
 @Configuration
-@ConfigurationProperties
 public class GsonConfiguration {
 
   @Bean
