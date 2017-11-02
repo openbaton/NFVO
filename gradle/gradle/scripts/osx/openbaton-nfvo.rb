6381e209afe87b8263f8b8f43f6411cc950f244a
Marcello Monachesi <marco.mantis@gmail.com>
2017-11-02 14:43:58 +0100

    fix: renaming application.properties

--- gradle/gradle/scripts/osx/openbaton-nfvo.rb
+++ gradle/gradle/scripts/osx/openbaton-nfvo.rb
@@ -8,20 +8,18 @@ class OpenbatonNfvo < Formula
   depends_on "rabbitmq"
 
   def install
-    system "./gradlew", "installDist"
+    system "./gradlew", "build", "-x", "test","installDist"
     # Change external path with /usr/local/etc instead /etc (due to brew internal directories)
     inreplace "build/install/openbaton-nfvo/bin/openbaton-nfvo" do |s|
       s.gsub! /^DEFAULT_CONFIG_FILE_PATH=.*/, "DEFAULT_CONFIG_FILE_PATH=#{etc}/openbaton/openbaton-nfvo.properties"
       s.gsub! /APP_HOME="`pwd -P`"/, %Q(APP_HOME="#{libexec}")
     end
-    # inreplace "build/install/openbaton-nfvo/bin/openbaton-nfvo", "DEFAULT_CONFIG_FILE_PATH=/etc/openbaton/openbaton-nfvo.properties", "DEFAULT_CONFIG_FILE_PATH=#{etc}/openbaton/openbaton-nfvo.properties"
-    # Change application path
-    # inreplace "build/install/openbaton-nfvo/bin/openbaton-nfvo", /APP_HOME="`pwd -P`"/, %Q(APP_HOME="#{libexec}")
 
     # Copy the openbaton-nfvo.properties in the right location
     openbaton_properties_path = etc+"openbaton"
     openbaton_properties_path.mkpath
     openbaton_properties_path.install "main/src/main/resources/application.properties"
+    mv openbaton_properties_path/"application.properties", openbaton_properties_path/"openbaton-nfvo.properties"
 
     # Remove Windows file
     rm_f Dir["build/install/openbaton-nfvo/bin/*.bat"]
