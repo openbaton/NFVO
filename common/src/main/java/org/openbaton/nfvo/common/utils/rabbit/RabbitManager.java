0aabcdf90bd19afa06105bcaee3c15ee1031d235
Marcello Monachesi <marco.mantis@gmail.com>
2017-12-08 13:19:42 +0100

    fix: J2EE bad practices leftover debug code

--- common/src/main/java/org/openbaton/nfvo/common/utils/rabbit/RabbitManager.java
+++ common/src/main/java/org/openbaton/nfvo/common/utils/rabbit/RabbitManager.java
@@ -267,7 +267,9 @@ public class RabbitManager {
     return factory;
   }
 
-  public static void main(String[] args) throws IOException {
-    System.out.println(getQueues("localhost", "admin", "openbaton", "/", 5672));
-  }
+  /*
+    public static void main(String[] args) throws IOException {
+      System.out.println(getQueues("localhost", "admin", "openbaton", "/", 5672));
+    }
+  */
 }
