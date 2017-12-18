b37a379db11c0bcfd22b8bab01010f0c039e2c95
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-12-18 16:44:42 +0100

    imp add getter and setter for registration class

--- registration/src/main/java/org/openbaton/registration/Registration.java
+++ registration/src/main/java/org/openbaton/registration/Registration.java
@@ -35,6 +35,22 @@ public class Registration {
   private static Logger log = LoggerFactory.getLogger(Registration.class);
   private Gson gson;
 
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
   private String username;
   private String password;
 
