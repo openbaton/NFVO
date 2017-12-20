3025b55b6c03ce0be68c18ff07ad16f69c70935e
mah88 <a.medhat.h@gmail.com>
2017-12-20 15:22:24 +0100

    imp: support HA and monitoring paramater in TOSCA

--- tosca-parser/src/main/java/org/openbaton/tosca/templates/TopologyTemplate/Nodes/VDU/VDUHighAvailability.java
+++ tosca-parser/src/main/java/org/openbaton/tosca/templates/TopologyTemplate/Nodes/VDU/VDUHighAvailability.java
@@ -1,4 +1,24 @@
 package org.openbaton.tosca.templates.TopologyTemplate.Nodes.VDU;
 
 public class VDUHighAvailability {
+    private HighAvailability highAvailability;
+
+  @SuppressWarnings({"unsafe", "unchecked"})
+  public VDUHighAvailability(Object o) {
+    highAvailability = new HighAvailability();
+
+    Map<String, Object> haMap = (Map<String, Object>) o;
+
+    if (haMap.containsKey("resiliencyLevel")) {
+      highAvailability.setResiliencyLevel(
+          ResiliencyLevel.valueOf(((String) haMap.get("resiliencyLevel")).toUpperCase()));
+    }
+    if (haMap.containsKey("redundancyScheme")) {
+      highAvailability.setRedundancyScheme((String) haMap.get("redundancyScheme"));
+    }
+  }
+
+  public HighAvailability getHighAvailability() {
+    return highAvailability;
+  }
 }
