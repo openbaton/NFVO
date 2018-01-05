c189bc517f8055c12fc8771c3dd0200c1c394538
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2018-01-05 13:05:29 +0100

    reformat code

--- vnfm/vnfm-impl/src/main/java/org/openbaton/nfvo/vnfm_reg/VnfmManager.java
+++ vnfm/vnfm-impl/src/main/java/org/openbaton/nfvo/vnfm_reg/VnfmManager.java
@@ -19,7 +19,6 @@ package org.openbaton.nfvo.vnfm_reg;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
@@ -84,7 +83,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
-/** Created by lto on 08/07/15. */
 @Service
 @Scope
 @Order(value = (Ordered.LOWEST_PRECEDENCE - 10)) // in order to be the second to last
@@ -112,14 +110,7 @@ public class VnfmManager
 
   private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
     List<Entry<K, V>> list = new LinkedList<>(map.entrySet());
-    Collections.sort(
-        list,
-        new Comparator<Entry<K, V>>() {
-          @Override
-          public int compare(Entry<K, V> o1, Entry<K, V> o2) {
-            return (o1.getValue()).compareTo(o2.getValue());
-          }
-        });
+    list.sort(Comparator.comparing(o -> (o.getValue())));
 
     Map<K, V> result = new LinkedHashMap<>();
     for (Entry<K, V> entry : list) {
