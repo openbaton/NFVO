73be624861c9e12590d2d5ca9c5deec7eb4d0ed5
Marcello Monachesi <marco.mantis@gmail.com>
2018-08-21 16:03:03 +0200

    update copyright in headers

--- api/src/main/java/org/openbaton/nfvo/api/catalogue/RestVNFFG.java
+++ api/src/main/java/org/openbaton/nfvo/api/catalogue/RestVNFFG.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Open Baton (http://openbaton.org)
+ * Copyright (c) 2015-2018 Open Baton (http://openbaton.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
